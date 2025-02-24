45dc37f384b009ef771a4c20ed6b18260bc73179
Tim Hunter <tim.hunter@ecmwf.int>
2025-02-24 15:48:55 +0100

    adding wegen_utils

---
+++ src/wegen_utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+Utilities for the Weather Generator (WEGEN) package.
+"""
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+def stub():
+    _logger.info("This is a stub function.")
+    return None
