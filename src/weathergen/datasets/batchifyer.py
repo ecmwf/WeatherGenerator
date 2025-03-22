ecmwf/WeatherGenerator

commit 23435d2b210031cb8fd7a5a06a41c051ab3f7f29

Refactor MultiStreamDataSampler and data loaders  (#102)
* Initial commit to enable support for source and target channel
selection. This requires a substantial refactoring, which was anyway
necessary to modularize the code. In particular, the batch construction
for source and target has been moved completely to the Batchifyer, which
finally simplifies the MultiStreamDataSampler to a reasonable size. This
change should also facilitate the re-introduction of masked token
modeling.

* Simplified interface of ctor of MultiStreamDataSampler by passing config
and using it wherever possible.

* Cleaned up interface

* Adding more streams---but they need to properly integrated to ensure all
functionality is supported.

* - Fixed various issues in FESOM dataloader and made it compliant with changes in MultiStreamdataSampler
- ruff-ing

* Initial commit to enable support for source and target channel
selection. This requires a substantial refactoring, which was anyway
necessary to modularize the code. In particular, the batch construction
for source and target has been moved completely to the Batchifyer, which
finally simplifies the MultiStreamDataSampler to a reasonable size. This
change should also facilitate the re-introduction of masked token
modeling.

* Simplified interface of ctor of MultiStreamDataSampler by passing config
and using it wherever possible.

* Cleaned up interface

* Adding more streams---but they need to properly integrated to ensure all
functionality is supported.

* - Fixed various issues in FESOM dataloader and made it compliant with changes in MultiStreamdataSampler
- ruff-ing

* Updates

src/weathergen/datasets/batchifyer.py
@@ -12,6 +12,7 @@ from functools import partial
 
 import astropy_healpix as hp
 import numpy as np
+import pandas as pd
 import torch
 from astropy_healpix.healpy import ang2pix
 
@@ -24,17 +25,72 @@ from weathergen.datasets.utils import (
 )
 
 
-#############################################
+def encode_times_source(times, time_win) -> torch.tensor:
+    # assemble tensor as fed to the network, combining geoinfo and data
+    fp32 = torch.float32
+    dt = pd.to_datetime(times)
+    dt_win = pd.to_datetime(time_win)
+    dt_delta = dt - dt_win[0]
+    time_tensor = torch.cat(
+        (
+            torch.tensor(dt.year, dtype=fp32).unsqueeze(1),
+            torch.tensor(dt.dayofyear, dtype=fp32).unsqueeze(1),
+            torch.tensor(dt.hour * 60 + dt.minute, dtype=fp32).unsqueeze(1),
+            torch.tensor(dt_delta.seconds, dtype=fp32).unsqueeze(1),
+            torch.tensor(dt_delta.seconds, dtype=fp32).unsqueeze(1),
+        ),
+        1,
+    )
+
+    # normalize
+    time_tensor[..., 0] /= 2100.0
+    time_tensor[..., 1] = time_tensor[..., 1] / 365.0
+    time_tensor[..., 2] = time_tensor[..., 2] / 1440.0
+    time_tensor[..., 3] = np.sin(time_tensor[..., 3] / (12.0 * 3600.0) * 2.0 * np.pi)
+    time_tensor[..., 4] = np.cos(time_tensor[..., 4] / (12.0 * 3600.0) * 2.0 * np.pi)
+
+    return time_tensor
+
+
+def encode_times_target(times, time_win) -> torch.tensor:
+    dt = pd.to_datetime(times)
+    dt_win = pd.to_datetime(time_win)
+    # for target only provide local time
+    dt_delta = torch.tensor((dt - dt_win[0]).seconds, dtype=torch.float32).unsqueeze(1)
+    time_tensor = torch.cat(
+        (
+            dt_delta,
+            dt_delta,
+            dt_delta,
+            dt_delta,
+            dt_delta,
+        ),
+        1,
+    )
+
+    # normalize
+    time_tensor[..., 0] = np.sin(time_tensor[..., 0] / (12.0 * 3600.0) * 2.0 * np.pi)
+    time_tensor[..., 1] = np.cos(time_tensor[..., 1] / (12.0 * 3600.0) * 2.0 * np.pi)
+    time_tensor[..., 2] = np.sin(time_tensor[..., 2] / (12.0 * 3600.0) * 2.0 * np.pi)
+    time_tensor[..., 3] = np.cos(time_tensor[..., 3] / (12.0 * 3600.0) * 2.0 * np.pi)
+    time_tensor[..., 4] = np.sin(time_tensor[..., 4] / (12.0 * 3600.0) * 2.0 * np.pi)
+
+    return time_tensor
+
+
 def tokenize_window_space(
+    stream_id,
+    coords,
+    geoinfos,
     source,
     times,
-    normalize_coords,
     tokens_cells,
+    time_win,
     token_size,
     hl,
-    geoinfo_offset,
     hpy_verts_Rs,
     rng,
+    normalizer,
     mr,
 ):
     """Process one window into tokens"""
@@ -43,8 +99,8 @@ def tokenize_window_space(
     if len(source) < 2:
         return tokens_cells
 
-    thetas = ((90.0 - source[:, geoinfo_offset]) / 180.0) * np.pi
-    phis = ((source[:, geoinfo_offset + 1] + 180.0) / 360.0) * 2.0 * np.pi
+    thetas = ((90.0 - coords[:, 0]) / 180.0) * np.pi
+    phis = ((coords[:, 1] + 180.0) / 360.0) * 2.0 * np.pi
     posr3 = s2tor3(thetas, phis)
     hpy_idxs = ang2pix(2**hl, thetas, phis, nest=True)
 
@@ -56,19 +112,33 @@ def tokenize_window_space(
     hpy_idxs_ord_split = np.split(hpy_idxs_ord, splits + 1)
 
     for i, c in enumerate(cells_idxs):
+        # use sorting based on theta
         thetas_sorted = torch.argsort(thetas[hpy_idxs_ord_split[i]], stable=True)
         posr3_cell = posr3[hpy_idxs_ord_split[i]][thetas_sorted]
-        source_cell = source[hpy_idxs_ord_split[i]][thetas_sorted]
-
         R = hpy_verts_Rs[c]
-        local_coords = r3tos2(torch.matmul(R, posr3_cell.transpose(1, 0)).transpose(1, 0))
-        source_cell[:, geoinfo_offset : geoinfo_offset + 2] = local_coords.to(torch.float32)
-        source_cell = normalize_coords(source_cell, False)
+        coords_cell = r3tos2(torch.matmul(R, posr3_cell.transpose(1, 0)).transpose(1, 0)).to(
+            torch.float32
+        )
 
-        # split into tokens and pad last one to have full size
+        source_cell = torch.cat(
+            (
+                torch.full([coords_cell.shape[0], 1], stream_id, dtype=torch.float32),
+                # use np.take to retain array and collapse it if index is 1-element
+                encode_times_source(
+                    np.take(np.take(times, hpy_idxs_ord_split[i]), thetas_sorted), time_win
+                ),
+                normalizer.normalize_coords(coords_cell),
+                normalizer.normalize_geoinfos(geoinfos[hpy_idxs_ord_split[i]][thetas_sorted]),
+                normalizer.normalize_source_channels(source[hpy_idxs_ord_split[i]][thetas_sorted]),
+            ),
+            1,
+        )
+
+        # split into tokens for current cells and pad last one to have full size
         pad = (
             token_size - (len(source_cell) % token_size) if len(source_cell) % token_size > 0 else 0
         )
+
         source_cell = torch.nn.functional.pad(
             source_cell, (0, 0, 0, pad), mode="constant", value=0.0
         )
@@ -88,30 +158,35 @@ def tokenize_window_space(
 
 #############################################
 def tokenize_window_spacetime(
+    stream_id,
+    coords,
+    geoinfos,
     source,
     times,
-    normalize_coords,
     tokens_cells,
+    time_win,
     token_size,
     hl,
-    geoinfo_offset,
     hpy_verts_Rs,
     rng,
+    normalizer,
     mr,
 ):
     t_unique = np.unique(times)
     for _, t in enumerate(t_unique):
         mask = t == times
         tokens_cells = tokenize_window_space(
+            coords[mask],
+            geoinfos[mask],
             source[mask],
             None,
-            normalize_coords,
             tokens_cells,
+            time_win,
             token_size,
             hl,
-            geoinfo_offset,
             hpy_verts_Rs,
             rng,
+            normalizer,
             mr,
         )
 
@@ -215,14 +290,15 @@ class Batchifyer:
     def batchify_source(
         self,
         stream_info,
-        geoinfo_offset,
-        geoinfo_size,
         masking_rate,
         masking_rate_sampling,
         rng,
+        coords,
+        geoinfos,
         source,
         times,
-        normalize_coords,
+        time_win,
+        normalizer,
     ):
         si = stream_info
         token_size = si["token_size"]
@@ -244,32 +320,31 @@ class Batchifyer:
                 masking_rate = 1.0 if self.rng.uniform() < masking_rate else 0.0
 
         tokenize_window = partial(
-            tokenize_window_space,
+            tokenize_window_spacetime if tokenize_spacetime else tokenize_window_space,
+            time_win=time_win,
             token_size=token_size,
             hl=self.hl_source,
-            geoinfo_offset=geoinfo_offset,
             hpy_verts_Rs=self.hpy_verts_Rs_source[-1],
+            normalizer=normalizer,
         )
-        if tokenize_spacetime:
-            tokenize_window = partial(
-                tokenize_window_spacetime,
-                token_size=token_size,
-                hl=self.hl_source,
-                geoinfo_offset=geoinfo_offset,
-                hpy_verts_Rs=self.hpy_verts_Rs_source[-1],
-            )
 
-        # source
-
-        if is_diagnostic or len(source) < 2 or masking_rate == 1.0:
+        if is_diagnostic or source.shape[1] == 0 or len(source) < 2 or masking_rate == 1.0:
             source_tokens_cells = torch.tensor([])
             source_centroids = torch.tensor([])
             source_tokens_lens = torch.zeros([self.num_healpix_cells_source], dtype=torch.int32)
 
         else:
             source_tokens_cells = [[] for _ in range(self.num_healpix_cells_source)]
+            # TODO: properly set stream_id; don't forget to normalize
             source_tokens_cells = tokenize_window(
-                source, times, normalize_coords, source_tokens_cells, rng=self.rng, mr=masking_rate
+                0,
+                coords,
+                geoinfos,
+                source,
+                times,
+                source_tokens_cells,
+                rng=self.rng,
+                mr=masking_rate,
             )
 
             source_tokens_cells = [
@@ -303,21 +378,22 @@ class Batchifyer:
     def batchify_target(
         self,
         stream_info,
-        geoinfo_offset,
-        geoinfo_size,
         sampling_rate_target,
         rng,
+        coords,
+        geoinfos,
         source,
-        times2,
-        normalize_targets,
+        times,
+        time_win,
+        normalizer,
     ):
         if len(source) < 2:
             target_tokens, target_coords = torch.tensor([]), torch.tensor([])
             target_tokens_lens = torch.zeros([self.num_healpix_cells_target], dtype=torch.int32)
 
         else:
-            thetas = ((90.0 - source[:, geoinfo_offset]) / 180.0) * np.pi
-            phis = ((source[:, geoinfo_offset + 1] + 180.0) / 360.0) * 2.0 * np.pi
+            thetas = ((90.0 - coords[:, 0]) / 180.0) * np.pi
+            phis = ((coords[:, 1] + 180.0) / 360.0) * 2.0 * np.pi
             hpy_idxs = ang2pix(2**self.hl_target, thetas, phis, nest=True)
             hpy_idxs_ord = np.argsort(hpy_idxs)
 
@@ -328,14 +404,19 @@ class Batchifyer:
             )
             hpy_idxs_ord_split = np.split(hpy_idxs_ord, splits + 1)
 
+            times = encode_times_target(times, time_win)
+
             target_tokens = [torch.tensor([]) for _ in range(self.num_healpix_cells_target)]
             target_coords = [torch.tensor([]) for _ in range(self.num_healpix_cells_target)]
+            target_geoinfos = [torch.tensor([]) for _ in range(self.num_healpix_cells_target)]
+            target_times = [torch.tensor([]) for _ in range(self.num_healpix_cells_target)]
             for i, c in enumerate(cells_idxs):
-                t = source[hpy_idxs_ord_split[i]]
+                t = normalizer.normalize_target_channels(source[hpy_idxs_ord_split[i]])
                 t = t[self.rng.permutation(len(t))][: int(len(t) * sampling_rate_target)]
                 target_tokens[c] = t
-                # target_coords[c] = normalize_coords(t[:,:geoinfo_size].clone(), False)
-                target_coords[c] = normalize_targets(t[:, :geoinfo_size].clone())
+                target_coords[c] = coords[hpy_idxs_ord_split[i]]
+                target_geoinfos[c] = normalizer.normalize_geoinfos(geoinfos[hpy_idxs_ord_split[i]])
+                target_times[c] = times[hpy_idxs_ord_split[i]]
 
             target_tokens_lens = torch.tensor([len(s) for s in target_tokens], dtype=torch.int32)
 
@@ -344,7 +425,8 @@ class Batchifyer:
                 target_coords = get_target_coords_local_ffast(
                     self.hl_target,
                     target_coords,
-                    geoinfo_offset,
+                    target_geoinfos,
+                    target_times,
                     self.hpy_verts_Rs_target,
                     self.hpy_verts_local_target,
                     self.hpy_nctrs_target,

config/streams/streams_ocean/fesom.yml
@@ -7,20 +7,19 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-ERA5 :
-  type : anemoi
-  filenames : ['aifs-ea-an-oper-0001-mars-o96-1979-2022-6h-v6.zarr']
-  # source : ['u_', 'v_', '10u', '10v']
-  # target : ['10u', '10v']
+FESOM :
+  type : fesom
+  filenames : ['coupled_yearly']
   loss_weight : 1.
-  diagnostic : False
+  source : null
+  target : ['sst']
   masking_rate : 0.6
   masking_rate_none : 0.05
-  token_size : 32
+  token_size : 64
   embed : 
     net : transformer
     num_tokens : 1
-    num_heads : 8
+    num_heads : 2
     dim_embed : 256
     num_blocks : 2
   embed_target_coords :

src/weathergen/datasets/anemoi_dataset.py
@@ -18,14 +18,15 @@ class AnemoiDataset:
 
     def __init__(
         self,
-        filename: str,
         start: int,
         end: int,
         len_hrs: int,
-        step_hrs: int = None,
-        normalize: bool = True,
-        select: list[str] = None,
+        step_hrs: int,
+        filename: str,
+        stream_info: dict,
     ) -> None:
+        # TODO: add support for different normalization modes
+
         assert len_hrs == step_hrs, "Currently only step_hrs=len_hrs is supported"
 
         # open  dataset to peak that it is compatible with requested parameters
@@ -40,30 +41,61 @@ class AnemoiDataset:
         dt_start = datetime.datetime.strptime(str(start), format_str)
         dt_end = datetime.datetime.strptime(str(end), format_str)
 
+        # TODO, TODO, TODO: we need proper alignment for the case where self.ds.frequency
+        # is not a multile of len_hrs
+        self.num_steps_per_window = int((len_hrs * 3600) / self.ds.frequency.seconds)
+
         # open dataset
 
         # caches lats and lons
         self.latitudes = self.ds.latitudes.astype(np.float32)
         self.longitudes = self.ds.longitudes.astype(np.float32)
 
-        # find physical fields (i.e. filter out auxiliary information to facilitate prediction)
-        self.fields_idx = np.sort(
+        # TODO: define in base class
+        self.geoinfo_idx = []
+
+        # Determine source and target channels, filtering out forcings etc and using
+        # specified source and target channels if specified
+        source_channels = stream_info["source"] if "source" in stream_info else None
+        self.source_idx = np.sort(
             [
                 self.ds.name_to_index[k]
                 for i, (k, v) in enumerate(self.ds.typed_variables.items())
-                if not v.is_computed_forcing and not v.is_constant_in_time
+                if (
+                    not v.is_computed_forcing
+                    and not v.is_constant_in_time
+                    and (
+                        np.array([f in k for f in source_channels]).any()
+                        if source_channels
+                        else True
+                    )
+                )
             ]
         )
-        # TODO: use complement of self.fields_idx as geoinfo
-        self.fields = [self.ds.variables[i] for i in self.fields_idx]
-        self.colnames = ["lat", "lon"] + self.fields
-        self.selected_colnames = self.colnames
+        target_channels = stream_info["target"] if "target" in stream_info else None
+        self.target_idx = np.sort(
+            [
+                self.ds.name_to_index[k]
+                for i, (k, v) in enumerate(self.ds.typed_variables.items())
+                if (
+                    not v.is_computed_forcing
+                    and not v.is_constant_in_time
+                    and (
+                        np.array([f in k for f in target_channels]).any()
+                        if target_channels
+                        else True
+                    )
+                )
+            ]
+        )
+        self.source_channels = [self.ds.variables[i] for i in self.source_idx]
+        self.target_channels = [self.ds.variables[i] for i in self.target_idx]
 
         self.properties = {
-            "obs_id": 0,
-            "means": self.ds.statistics["mean"],
-            "vars": np.square(self.ds.statistics["stdev"]),
+            "stream_id": 0,
         }
+        self.mean = self.ds.statistics["mean"]
+        self.stdev = self.ds.statistics["stdev"]
 
         # set dataset to None when no overlap with time range
         if dt_start >= ds_dt_end or dt_end <= ds_dt_start:
@@ -80,26 +112,128 @@ class AnemoiDataset:
 
         return len(self.ds)
 
-    def __getitem__(self, idx: int) -> tuple:
-        "Get (data,datetime) for given index"
+    def get_source(self, idx: int) -> tuple[np.array, np.array, np.array, np.array]:
+        """
+        TODO
+        """
+        return self._get(idx, self.source_idx)
+
+    def get_target(self, idx: int) -> tuple[np.array, np.array, np.array, np.array]:
+        """
+        TODO
+        """
+        return self._get(idx, self.target_idx)
+
+    def _get(
+        self, idx: int, channels_idx: np.array
+    ) -> tuple[np.array, np.array, np.array, np.array]:
+        """
+        TODO
+        """
 
         if not self.ds:
-            return (np.array([], dtype=np.float32), np.array([], dtype=np.float32))
+            return (
+                np.array([], dtype=np.float32),
+                np.array([], dtype=np.float32),
+                np.array([], dtype=np.float32),
+                np.array([], dtype=np.float32),
+            )
 
-        # prepend lat and lon to data; squeeze out ensemble dimension (for the moment)
-        data = np.concatenate(
+        # extract number of time steps and collapse ensemble dimension
+        data = self.ds[idx : idx + self.num_steps_per_window][:, :, 0]
+        # extract channels
+        data = (
+            data[:, channels_idx].transpose([0, 2, 1]).reshape((data.shape[0] * data.shape[2], -1))
+        )
+
+        # construct lat/lon coords
+        latlon = np.concatenate(
             [
                 np.expand_dims(self.latitudes, 0),
                 np.expand_dims(self.longitudes, 0),
-                self.ds[idx].squeeze(),
             ],
             0,
         ).transpose()
+        latlon = np.repeat(latlon, self.num_steps_per_window, axis=0).reshape((-1, latlon.shape[1]))
+
+        # empty geoinfos for anemoi
+        geoinfos = np.zeros((data.shape[0], 0), dtype=data.dtype)
 
         # date time matching #data points of data
-        datetimes = np.full(data.shape[0], self.ds.dates[idx])
+        datetimes = np.repeat(
+            np.expand_dims(self.ds.dates[idx : idx + self.num_steps_per_window], 0),
+            data.shape[0],
+            axis=0,
+        ).flatten()
 
-        return (data, datetimes)
+        return (latlon, geoinfos, data, datetimes)
+
+    def get_source_size(self):
+        """
+        TODO
+        """
+        return 2 + len(self.geoinfo_idx) + len(self.source_idx)
+
+    def get_source_num_channels(self):
+        """
+        TODO
+        """
+        return len(self.source_idx)
+
+    def get_target_size(self):
+        """
+        TODO
+        """
+        return 2 + len(self.geoinfo_idx) + len(self.target_idx)
+
+    def get_target_num_channels(self):
+        """
+        TODO
+        """
+        return len(self.target_idx)
+
+    def get_geoinfo_size(self):
+        """
+        TODO
+        """
+        return len(self.geoinfo_idx)
+
+    def normalize_coords(self, coords):
+        """
+        TODO
+        """
+        coords[..., 0] = np.sin(np.deg2rad(coords[..., 0]))
+        coords[..., 1] = np.sin(0.5 * np.deg2rad(coords[..., 1]))
+
+        return coords
+
+    def normalize_geoinfos(self, geoinfos):
+        """
+        TODO
+        """
+
+        assert geoinfos.shape[-1] == 0
+        return geoinfos
+
+    def normalize_source_channels(self, source):
+        """
+        TODO
+        """
+        assert source.shape[1] == len(self.source_idx)
+        for i, ch in enumerate(self.source_idx):
+            source[..., i] = (source[..., i] - self.mean[ch]) / self.stdev[ch]
+
+        return source
+
+    def normalize_target_channels(self, target):
+        """
+        TODO
+        """
+        assert target.shape[1] == len(self.target_idx)
+        for i, ch in enumerate(self.target_idx):
+            target[..., i] = (target[..., i] - self.mean[ch]) / self.stdev[ch]
+
+        return target
 
     def time_window(self, idx: int) -> tuple[np.datetime64, np.datetime64]:
         if not self.ds:

src/weathergen/datasets/atmorep_dataset.py
@@ -1,0 +1,89 @@
+from datetime import datetime
+
+import numpy as np
+import zarr
+
+
+class AtmorepDataset:
+    def __init__(
+        self,
+        filename: str,
+        start: datetime | int,
+        end: datetime | int,
+        len_hrs: int,
+        step_hrs: int | None = None,
+        normalize: bool = True,
+        select: list[str] | None = None,
+    ):
+        format_str = "%Y%m%d%H%M%S"
+        if type(start) is int:
+            start = datetime.strptime(str(start), format_str)
+
+        if type(end) is int:
+            end = datetime.strptime(str(end), format_str)
+
+        self.normalize = normalize
+        self.filename = filename
+        self.z = zarr.open(filename, mode="r")
+
+        self.lats, self.lons = np.meshgrid(np.array(self.z["lats"]), np.array(self.z["lons"]))
+        self.lats = self.lats.flatten()
+        self.lons = self.lons.flatten()
+        # Reshape lats and lons to be in shape (1, len_hrs, size_lat * size_lon), ready to added to data
+        self.lats = np.expand_dims(np.stack((self.lats,) * len_hrs, axis=1).T, 0)
+        self.lons = np.expand_dims(np.stack((self.lons,) * len_hrs, axis=1).T, 0)
+
+        self.time = np.array(self.z["time"], dtype=np.datetime64)
+        self.start_idx = np.searchsorted(self.time, start)
+        self.end_idx = np.searchsorted(self.time, end)
+
+        assert self.end_idx > self.start_idx, (
+            f"Abort: Final index of {self.end_idx} is the same of larger than start index {self.start_idx}"
+        )
+
+        self.colnames = ["lat", "lon"] + list(self.z.attrs["fields"])
+        self.len_hrs = len_hrs
+        # Ignore step_hrs, idk how it supposed to work
+        self.step_hrs = 1
+
+        self.selected_colnames = self.colnames[2:]
+        self.selected_cols_idx = np.arange(len(self.selected_colnames))
+        self.data = self.z["data"]
+
+        self.properties = {
+            "obs_id": 0,
+            "means": np.zeros(len(self.colnames), dtype=np.float32),
+            "vars": np.ones(len(self.colnames), dtype=np.float32),
+        }
+
+        if select:
+            self.select(select)
+
+    def select(self, cols_list: list[str]) -> None:
+        """
+        Allow user to specify which columns they want to access.
+        Get functions only returned for these specified columns.
+        """
+        self.selected_colnames = cols_list
+        self.selected_cols_idx = np.array([self.colnames.index(item) for item in cols_list])
+
+    def __len__(self):
+        return self.end_idx - self.start_idx - self.len_hrs
+
+    def __getitem__(self, idx: int) -> tuple:
+        start_row = self.start_idx + idx
+        end_row = start_row + self.len_hrs
+
+        data = self.data.oindex[start_row:end_row, :, 0, :, :]
+        datetimes = np.tile(self.time[start_row:end_row], data.shape[-1] * data.shape[-2])
+
+        data = np.reshape(data, (data.shape[1], data.shape[0], -1))
+        data = np.concatenate([self.lats, self.lons, data], 0).T
+        data = np.reshape(data, (-1, data.shape[-1]))
+
+        return (data.astype(np.float32), datetimes)
+
+    def time_window(self, idx: int) -> tuple[np.datetime64, np.datetime64]:
+        start_row = self.start_idx + idx
+        end_row = start_row + self.len_hrs
+        return (self.time[start_row], self.time[end_row])

commit 11474ec90763fb2ed55da737e00d09da44ccfa09

Fixing bug introduced in PR #102 where target coords where no longer properly aligned with target.

src/weathergen/datasets/batchifyer.py
@@ -414,13 +414,13 @@ class Batchifyer:
             target_times = [torch.tensor([]) for _ in range(self.num_healpix_cells_target)]
             for i, c in enumerate(cells_idxs):
                 t = normalizer.normalize_target_channels(source[hpy_idxs_ord_split[i]])
-                t = t[self.rng.permutation(len(t))][: int(len(t) * sampling_rate_target)]
-                target_tokens[c] = t
-                target_coords[c] = coords[hpy_idxs_ord_split[i]]
-                target_coords_raw[c] = coords[hpy_idxs_ord_split[i]]
-                target_geoinfos[c] = normalizer.normalize_geoinfos(geoinfos[hpy_idxs_ord_split[i]])
-                target_times_raw[c] = times[hpy_idxs_ord_split[i]]
-                target_times[c] = times_enc[hpy_idxs_ord_split[i]]
+                perm = self.rng.permutation(len(t))[ : int(len(t) * sampling_rate_target)]
+                target_tokens[c] = t[perm]
+                target_coords[c] = coords[hpy_idxs_ord_split[i]][perm]
+                target_coords_raw[c] = coords[hpy_idxs_ord_split[i]][perm]
+                target_geoinfos[c] = normalizer.normalize_geoinfos(geoinfos[hpy_idxs_ord_split[i]][perm])
+                target_times_raw[c] = times[hpy_idxs_ord_split[i]][perm]
+                target_times[c] = times_enc[hpy_idxs_ord_split[i]][perm]
 
             target_tokens_lens = torch.tensor([len(s) for s in target_tokens], dtype=torch.int32)
 