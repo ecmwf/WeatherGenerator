Follow up fixes required by last commit with training_mode=masking.

src/weathergen/datasets/tokenizer_masking.py
@@ -281,7 +281,7 @@ class TokenizerMasking:
         target_tokens = [
             torch.cat([c if p else torch.tensor([]) for c, p in zip(cc, pp, strict=True)])
             if len(cc) > 0
-            else cc
+            else torch.tensor(cc)
             for cc, pp in zip(target_tokens_cells, self.perm_sel, strict=True)
         ]
         target_tokens_lens = [len(t) for t in target_tokens]