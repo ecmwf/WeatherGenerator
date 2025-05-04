22af0121f54fc6e0fb2ae1cb2ef36ddf228b7416
Savvas Melidonis <79579567+SavvasMel@users.noreply.github.com>
2025-05-04 20:48:11 +0200

    Fix for issue 168 regarding the coordinates in anemoi datasets (#182)
    * Ensure transform of coordinates to the standard tabular format that model expects
    
    * Add doc to the last changes
    
    * Pushing a more efficient code snippet

--- src/weathergen/datasets/anemoi_dataset.py
+++ src/weathergen/datasets/anemoi_dataset.py
@@ -78,6 +78,13 @@ class AnemoiDataset:
         self.latitudes = ds.latitudes.astype(np.float32)
         self.longitudes = ds.longitudes.astype(np.float32)
 
+        # Ensures that coordinates remain into the interval [-90,90] for latitudes
+        # and [-180, 180] for longitudes. Ensures that periodicity has been taken
+        # into consideration for the specific intervals.
+        self.latitudes = 2 * np.clip(self.latitudes, -90, 90) - self.latitudes
+
+        self.longitudes = (self.longitudes + 180) % 360 - 180
+
         # TODO: define in base class
         self.geoinfo_idx = []
 
