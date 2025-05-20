b617bf49905f191414c17e4cd68e7b9e2f3f6f82
Tim Hunter <tim.hunter@ecmwf.int>
2025-05-20 19:57:45 +0100

    changes

--- src/weathergen/run_train.py
+++ src/weathergen/run_train.py
@@ -4,12 +4,6 @@ The entry point to train the weathergen model.
 
 # For profiling tools, the entry point cannot be in an __init__.py file.
 from weathergen import train
-import os
 
 if __name__ == "__main__":
-    print("Starting training...")
-    # PRint the environment variables
-    print("Environment variables:")
-    for key, value in os.environ.items():
-        print(f"{key}: {value}")
     train()
