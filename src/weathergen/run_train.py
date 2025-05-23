7afb453ac7e3cae309e0c4742fec10c4e36be87c
Tim Hunter <tim.hunter@ecmwf.int>
2025-05-23 18:57:22 +0100

    working package management

--- src/weathergen/run_train.py
+++ src/weathergen/run_train.py
@@ -3,7 +3,15 @@ The entry point to train the weathergen model.
 """
 
 # For profiling tools, the entry point cannot be in an __init__.py file.
-from weathergen import train
+# from weathergen import train
+from weathergen.evaluation.test import test
+from weathergen.utils.my_file import my_fun
+
+def test_wrap():
+    print(test())
+    my_fun()
 
 if __name__ == "__main__":
-    train()
+    print("Running test...")
+    print(test())
+    # train()
