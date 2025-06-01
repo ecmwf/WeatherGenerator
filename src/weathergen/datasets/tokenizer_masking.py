2131d61efa80037e2c37827a711d1249166158a4
Christian Lessig <christian.lessig@ecmwf.int>
2025-06-01 10:11:29 +0000

    Fix division by zero

--- src/weathergen/datasets/tokenizer_masking.py
+++ src/weathergen/datasets/tokenizer_masking.py
@@ -133,7 +133,7 @@ class TokenizerMasking:
 
     def reset(self) -> None:
         worker_info = torch.utils.data.get_worker_info()
-        div_factor = worker_info.id if worker_info is not None else 1
+        div_factor = (worker_info.id + 1) if worker_info is not None else 1
         self.rng = np.random.default_rng(int(time.time() / div_factor))
 
     def batchify_source(
