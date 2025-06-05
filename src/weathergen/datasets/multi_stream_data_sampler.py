377752d2bf70d040918c8e89cd8103898f45b97b
sbAsma <semcheddineasma@yahoo.fr>
2025-06-05 08:29:30 +0200

    removed stats kwargs

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -94,7 +94,6 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                     case "icon":
                         dataset = IconDataset
                         datapath = cf.data_path_icon                        
-                        kwargs["normalization_file"] = stream_info.get("normalization_file")
                     case _:
                         msg = f"Unsupported stream type {stream_info['type']}"
                         raise ValueError(msg)
