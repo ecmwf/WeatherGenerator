Fix mixed precision training

src/weathergen/model/attention.py
@@ -70,29 +70,31 @@ class MultiSelfAttentionHead_Varlen(torch.nn.Module):
 
     #########################################
     def forward(self, x, x_lens, ada_ln_aux=None):
-        x_in = x
-        x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            x_in = x
+            x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [x.shape[0], self.num_heads, -1]
-        qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype)
-        ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype)
-        vs = self.proj_heads_v(x).reshape(s)
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [x.shape[0], self.num_heads, -1]
+            qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype)
+            ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype)
+            vs = self.proj_heads_v(x).reshape(s)
 
-        cum_x_lens = torch.cumsum(x_lens, 0, dtype=torch.int32)
-        # ordering of tensors (seq, heads, embed) (which differs from torch's flash attention implt)
-        outs = flash_attn_varlen_func(
-            qs,
-            ks,
-            vs,
-            cum_x_lens,
-            cum_x_lens,
-            x_lens.max(),
-            x_lens.max(),
-            softcap=self.softcap,
-            dropout_p=self.dropout_rate,
-        )
+            cum_x_lens = torch.cumsum(x_lens, 0, dtype=torch.int32)
+            # ordering of tensors (seq, heads, embed)
+            # (which differs from torch's flash attention implt)
+            outs = flash_attn_varlen_func(
+                qs,
+                ks,
+                vs,
+                cum_x_lens,
+                cum_x_lens,
+                x_lens.max(),
+                x_lens.max(),
+                softcap=self.softcap,
+                dropout_p=self.dropout_rate,
+            )
 
         # return x_in + self.dropout( self.proj_out( outs.flatten( -2, -1)) )
         return x_in + self.proj_out(outs.flatten(-2, -1))
@@ -155,17 +157,18 @@ class MultiSelfAttentionHead_Varlen_Flex(torch.nn.Module):
 
     #########################################
     def forward(self, x, x_lens=None):
-        x_in = x
-        x = self.lnorm(x)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            x_in = x
+            x = self.lnorm(x)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [x.shape[0], 1, self.num_heads, -1]
-        qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype).permute([1, 2, 0, 3])
-        ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype).permute([1, 2, 0, 3])
-        vs = self.proj_heads_v(x).reshape(s).permute([1, 2, 0, 3])
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [x.shape[0], 1, self.num_heads, -1]
+            qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype).permute([1, 2, 0, 3])
+            ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype).permute([1, 2, 0, 3])
+            vs = self.proj_heads_v(x).reshape(s).permute([1, 2, 0, 3])
 
-        outs = self.compiled_flex_attention(qs, ks, vs).transpose(1, 2).squeeze()
+            outs = self.compiled_flex_attention(qs, ks, vs).transpose(1, 2).squeeze()
 
         return x_in + self.dropout(self.proj_out(outs.flatten(-2, -1)))
 
@@ -234,16 +237,17 @@ class MultiSelfAttentionHead_Local(torch.nn.Module):
 
     #########################################
     def forward(self, x, ada_ln_aux=None):
-        x_in = x
-        x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            x_in = x
+            x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
 
-        # project onto heads
-        s = [x.shape[0], x.shape[1], self.num_heads, -1]
-        qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype).permute([0, 2, 1, 3])
-        ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype).permute([0, 2, 1, 3])
-        vs = self.proj_heads_v(x).reshape(s).permute([0, 2, 1, 3])
+            # project onto heads
+            s = [x.shape[0], x.shape[1], self.num_heads, -1]
+            qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype).permute([0, 2, 1, 3])
+            ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype).permute([0, 2, 1, 3])
+            vs = self.proj_heads_v(x).reshape(s).permute([0, 2, 1, 3])
 
-        outs = self.flex_attention(qs, ks, vs, block_mask=self.block_mask).transpose(1, 2)
+            outs = self.flex_attention(qs, ks, vs, block_mask=self.block_mask).transpose(1, 2)
 
         return x_in + self.proj_out(self.dropout(outs.flatten(-2, -1)))
 
@@ -310,40 +314,41 @@ class MultiCrossAttentionHead_Varlen(torch.nn.Module):
 
     #########################################
     def forward(self, x_q, x_kv, x_q_lens=None, x_kv_lens=None, ada_ln_aux=None):
-        if self.with_residual:
-            x_q_in = x_q
-        x_q = self.lnorm_in_q(x_q) if ada_ln_aux is None else self.lnorm_in_q(x_q, ada_ln_aux)
-        x_kv = self.lnorm_in_kv(x_kv)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            if self.with_residual:
+                x_q_in = x_q
+            x_q = self.lnorm_in_q(x_q) if ada_ln_aux is None else self.lnorm_in_q(x_q, ada_ln_aux)
+            x_kv = self.lnorm_in_kv(x_kv)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [x_q.shape[0], self.num_heads, self.dim_head_proj]
-        qs = self.lnorm_q(self.proj_heads_q(x_q).reshape(s)).to(self.dtype)
-        s = [x_kv.shape[0], self.num_heads, self.dim_head_proj]
-        ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype)
-        vs = self.proj_heads_v(x_kv).reshape(s)
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [x_q.shape[0], self.num_heads, self.dim_head_proj]
+            qs = self.lnorm_q(self.proj_heads_q(x_q).reshape(s)).to(self.dtype)
+            s = [x_kv.shape[0], self.num_heads, self.dim_head_proj]
+            ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype)
+            vs = self.proj_heads_v(x_kv).reshape(s)
 
-        if x_kv_lens is not None:
-            cum_x_q_lens = torch.cumsum(x_q_lens, 0, dtype=torch.int32)
-            cum_x_kv_lens = torch.cumsum(x_kv_lens, 0, dtype=torch.int32)
-            outs = flash_attn_varlen_func(
-                qs,
-                ks,
-                vs,
-                cum_x_q_lens,
-                cum_x_kv_lens,
-                x_q_lens.max(),
-                x_kv_lens.max(),
-                softcap=self.softcap,
-                dropout_p=self.dropout_rate,
-            )
-        else:
-            assert False
+            if x_kv_lens is not None:
+                cum_x_q_lens = torch.cumsum(x_q_lens, 0, dtype=torch.int32)
+                cum_x_kv_lens = torch.cumsum(x_kv_lens, 0, dtype=torch.int32)
+                outs = flash_attn_varlen_func(
+                    qs,
+                    ks,
+                    vs,
+                    cum_x_q_lens,
+                    cum_x_kv_lens,
+                    x_q_lens.max(),
+                    x_kv_lens.max(),
+                    softcap=self.softcap,
+                    dropout_p=self.dropout_rate,
+                )
+            else:
+                assert False
 
-        # outs = self.dropout( self.proj_out( outs.flatten( -2, -1)) )
-        outs = self.proj_out(outs.flatten(-2, -1))
-        if self.with_residual:
-            outs = x_q_in + outs
+            # outs = self.dropout( self.proj_out( outs.flatten( -2, -1)) )
+            outs = self.proj_out(outs.flatten(-2, -1))
+            if self.with_residual:
+                outs = x_q_in + outs
 
         return outs
 
@@ -427,44 +432,44 @@ class MultiCrossAttentionHead_Varlen_SlicedQ(torch.nn.Module):
 
     #########################################
     def forward(self, x_q, x_kv, x_q_lens=None, x_kv_lens=None, ada_ln_aux=None):
-        if self.with_residual:
-            x_q_in = x_q
-        x_q = self.lnorm_in_q(x_q) if ada_ln_aux is None else self.lnorm_in_q(x_q, ada_ln_aux)
-        x_kv = self.lnorm_in_kv(x_kv)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            if self.with_residual:
+                x_q_in = x_q
+            x_q = self.lnorm_in_q(x_q) if ada_ln_aux is None else self.lnorm_in_q(x_q, ada_ln_aux)
+            x_kv = self.lnorm_in_kv(x_kv)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [x_q.shape[0], self.num_heads, self.dim_head_proj]
-        qs = [
-            self.lnorm_q(head_proj(x_q_i).reshape(s)).to(self.dtype)
-            for head_proj, x_q_i in zip(self.proj_heads_q, x_q.transpose(1, 0), strict=False)
-        ]
-        s = [x_kv.shape[0], self.num_heads, self.dim_head_proj]
-        ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype)
-        vs = self.proj_heads_v(x_kv).reshape(s)
-
-        cum_x_q_lens = torch.cumsum(x_q_lens, 0, dtype=torch.int32)
-        cum_x_kv_lens = torch.cumsum(x_kv_lens, 0, dtype=torch.int32)
-        outs = []
-        for _i, qs_i in enumerate(qs):
-            outs += [
-                flash_attn_varlen_func(
-                    qs_i,
-                    ks,
-                    vs,
-                    cum_x_q_lens,
-                    cum_x_kv_lens,
-                    x_q_lens.max(),
-                    x_kv_lens.max(),
-                    softcap=self.softcap,
-                    dropout_p=self.dropout_rate,
-                )
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [x_q.shape[0], self.num_heads, self.dim_head_proj]
+            qs = [
+                self.lnorm_q(head_proj(x_q_i).reshape(s)).to(self.dtype)
+                for head_proj, x_q_i in zip(self.proj_heads_q, x_q.transpose(1, 0), strict=False)
             ]
+            s = [x_kv.shape[0], self.num_heads, self.dim_head_proj]
+            ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype)
+            vs = self.proj_heads_v(x_kv).reshape(s)
 
-        # outs = self.dropout( self.proj_out( torch.stack(outs).transpose(1,0).flatten( -2, -1)) )
-        outs = self.proj_out(torch.stack(outs).transpose(1, 0).flatten(-2, -1))
-        if self.with_residual:
-            outs = x_q_in + outs.reshape(x_q_in.shape)
+            cum_x_q_lens = torch.cumsum(x_q_lens, 0, dtype=torch.int32)
+            cum_x_kv_lens = torch.cumsum(x_kv_lens, 0, dtype=torch.int32)
+            outs = []
+            for _i, qs_i in enumerate(qs):
+                outs += [
+                    flash_attn_varlen_func(
+                        qs_i,
+                        ks,
+                        vs,
+                        cum_x_q_lens,
+                        cum_x_kv_lens,
+                        x_q_lens.max(),
+                        x_kv_lens.max(),
+                        softcap=self.softcap,
+                        dropout_p=self.dropout_rate,
+                    )
+                ]
+
+            outs = self.proj_out(torch.stack(outs).transpose(1, 0).flatten(-2, -1))
+            if self.with_residual:
+                outs = x_q_in + outs.reshape(x_q_in.shape)
 
         return outs
 
@@ -524,21 +529,23 @@ class MultiSelfAttentionHead(torch.nn.Module):
 
     #########################################
     def forward(self, x, ada_ln_aux=None):
-        x_in = x
-        # x = self.lnorm( x)
-        x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            x_in = x
+            # x = self.lnorm( x)
+            x = self.lnorm(x) if ada_ln_aux is None else self.lnorm(x, ada_ln_aux)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [*([x.shape[0], 1] if len(x.shape) == 2 else x.shape[:-1]), self.num_heads, -1]
-        qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype)
-        ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype)
-        vs = self.proj_heads_v(x).reshape(s).to(self.dtype)
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [*([x.shape[0], 1] if len(x.shape) == 2 else x.shape[:-1]), self.num_heads, -1]
+            qs = self.lnorm_q(self.proj_heads_q(x).reshape(s)).to(self.dtype)
+            ks = self.lnorm_k(self.proj_heads_k(x).reshape(s)).to(self.dtype)
+            vs = self.proj_heads_v(x).reshape(s).to(self.dtype)
 
-        # ordering of tensors (seq, heads, embed) (which differs from torch's flash attention implt)
-        outs = flash_attn_func(qs, ks, vs, dropout_p=self.dropout_rate)
+            # ordering of tensors (seq, heads, embed)
+            # (which differs from torch's flash attention implt)
+            outs = flash_attn_func(qs, ks, vs, dropout_p=self.dropout_rate)
 
-        # return x_in + self.dropout( self.proj_out( outs.flatten( -2, -1)) )
+            # return x_in + self.dropout( self.proj_out( outs.flatten( -2, -1)) )
         return x_in + self.proj_out(outs.flatten(-2, -1))
 
     #########################################
@@ -608,25 +615,26 @@ class MultiCrossAttentionHead(torch.nn.Module):
 
     #########################################
     def forward(self, x_q, x_kv):
-        if self.with_residual:
-            x_q_in = x_q
-        x_q, x_kv = self.lnorm_in_q(x_q), self.lnorm_in_kv(x_kv)
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16, enabled=True):
+            if self.with_residual:
+                x_q_in = x_q
+            x_q, x_kv = self.lnorm_in_q(x_q), self.lnorm_in_kv(x_kv)
 
-        ## project onto heads and q,k,v and
-        #  ensure these are 4D tensors as required for flash attention
-        s = [x_q.shape[0], -1, self.num_heads, self.dim_head_proj]
-        qs = self.lnorm_q(self.proj_heads_q(x_q).reshape(s)).to(self.dtype).transpose(-3, -2)
-        s = [x_kv.shape[0], -1, self.num_heads, self.dim_head_proj]
-        ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype).transpose(-3, -2)
-        vs = self.proj_heads_v(x_kv).reshape(s).transpose(-3, -2)
+            ## project onto heads and q,k,v and
+            #  ensure these are 4D tensors as required for flash attention
+            s = [x_q.shape[0], -1, self.num_heads, self.dim_head_proj]
+            qs = self.lnorm_q(self.proj_heads_q(x_q).reshape(s)).to(self.dtype).transpose(-3, -2)
+            s = [x_kv.shape[0], -1, self.num_heads, self.dim_head_proj]
+            ks = self.lnorm_k(self.proj_heads_k(x_kv).reshape(s)).to(self.dtype).transpose(-3, -2)
+            vs = self.proj_heads_v(x_kv).reshape(s).transpose(-3, -2)
 
-        # correct ordering of tensors with seq dimension second but last is critical
-        with torch.nn.attention.sdpa_kernel(torch.nn.attention.SDPBackend.FLASH_ATTENTION):
-            outs = self.att(qs, ks, vs).transpose(2, 1)
+            # correct ordering of tensors with seq dimension second but last is critical
+            with torch.nn.attention.sdpa_kernel(torch.nn.attention.SDPBackend.FLASH_ATTENTION):
+                outs = self.att(qs, ks, vs).transpose(2, 1)
 
-        outs = self.dropout(self.proj_out(outs.flatten(-2, -1)))
-        if self.with_residual:
-            outs = x_q_in + outs
+            outs = self.dropout(self.proj_out(outs.flatten(-2, -1)))
+            if self.with_residual:
+                outs = x_q_in + outs
 
         return outs
 

src/weathergen/model/embeddings.py
@@ -9,7 +9,6 @@
 
 import numpy as np
 import torch
-from deepspeed.runtime.activation_checkpointing.checkpointing import checkpoint
 
 from weathergen.model.attention import MultiSelfAttentionHead
 from weathergen.model.layers import MLP
@@ -146,17 +145,17 @@ class StreamEmbedTransformer(torch.nn.Module):
 
         with torch.amp.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
             # embed provided input data
-            x = peh(checkpoint(self.embed, x_in.transpose(-2, -1)))
+            x = peh(self.embed(x_in.transpose(-2, -1)))
 
             for layer in self.layers:
-                x = checkpoint(layer, x)
+                x = layer(x)
 
             # read out
             if self.unembed_mode == "full":
-                out = checkpoint(self.unembed, self.ln_final(x.flatten(-2, -1)))
+                out = self.unembed(self.ln_final(x.flatten(-2, -1)))
             elif self.unembed_mode == "block":
                 out = [
-                    checkpoint(ue, ln(x[:, i]))
+                    ue(ln(x[:, i]))
                     for i, (ue, ln) in enumerate(zip(self.unembed, self.ln_final, strict=True))
                 ]
                 out = torch.stack(out, dim=1).flatten(-2, -1)
@@ -178,14 +177,14 @@ class StreamEmbedTransformer(torch.nn.Module):
     def forward_columns(self, x_in, centroids):
         # embed provided input data
         with torch.amp.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
-            x = positional_encoding_harmonic(checkpoint(self.embed, x_in))
+            x = positional_encoding_harmonic(self.embed(x_in))
 
             for layer in self.layers:
-                x = checkpoint(layer, x)
+                x = layer(x)
 
-            out = checkpoint(self.unembed1, x)
+            out = self.unembed1(x)
             out = self.unembed_nonlin(out)
-            out = checkpoint(self.unembed2, out.transpose(-2, -1))
+            out = self.unembed2(out.transpose(-2, -1))
             out = out.flatten(-2, -1).unsqueeze(1)
 
             # final normalize and dropout

src/weathergen/model/engines.py
@@ -9,6 +9,7 @@
 
 import logging
 import torch
+import torch.nn.functional as F
 
 from weathergen.model.attention import (
     MultiCrossAttentionHead_Varlen,
@@ -343,21 +344,31 @@ class EnsPredictionHead(torch.nn.Module):
                 )
 
     #########################################
-    @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")
+    # @torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")
     def forward(self, toks):
-        with torch.autocast(device_type="cuda", enabled=False):
-            preds = []
-            # toks = toks.to(torch.float32)
-            _logger.debug(f"tokens dtype: {toks.dtype}")
+        preds = []
+        toks = toks.to(torch.float32)
+        # _logger.debug(f"tokens dtype: {toks.dtype}")
 
-            for pred_head in self.pred_heads:
-                cpred = toks
-                for block in pred_head:
-                    _logger.debug(f"block dtype: {block.weight.dtype}")
+        for pred_head in self.pred_heads:
+            cpred = toks
+            for block in pred_head:
+                if isinstance(block, torch.nn.Linear):
+                    # Manually cast the layer's bfloat16 weight and bias to
+                    # float32 to match the float32 input 'cpred'.
+                    weight_fp32 = block.weight.to(torch.float32)
+                    bias_fp32 = block.bias.to(torch.float32) if block.bias is not None else None
+                    # _logger.debug(f"block dtype: {weight_fp32.dtype}")
+
+                    cpred = F.linear(cpred, weight_fp32, bias_fp32)
+                else:
+                    # For non-Linear layers like GELU, just call them normally.
                     cpred = block(cpred)
-                preds.append(cpred)
-            preds = torch.stack(preds, 0)
-            _logger.debug(f"preds dtype: {preds.dtype}")
+                # _logger.debug(f"cpred dtype: {cpred.dtype}")
+            preds.append(cpred)
+        preds = torch.stack(preds, 0)
+        # _logger.debug(f"preds dtype: {preds.dtype}")
+
         return preds
 
 

src/weathergen/model/layers.py
@@ -62,16 +62,17 @@ class MLP(torch.nn.Module):
         self.layers.append(nonlin())
 
     def forward(self, *args):
-        x, x_in, aux = args[0], args[0], args[-1]
+        with torch.autocast(enabled=True, dtype=torch.bfloat16, device_type="cuda"):
+            x, x_in, aux = args[0], args[0], args[-1]
 
-        for i, layer in enumerate(self.layers):
-            x = layer(x, aux) if (i == 0 and self.with_aux) else layer(x)
+            for i, layer in enumerate(self.layers):
+                x = layer(x, aux) if (i == 0 and self.with_aux) else layer(x)
 
-        if self.with_residual:
-            if x.shape[-1] == x_in.shape[-1]:
-                x = x_in + x
-            else:
-                assert x.shape[-1] % x_in.shape[-1] == 0
-                x = x + x_in.repeat([*[1 for _ in x.shape[:-1]], x.shape[-1] // x_in.shape[-1]])
+            if self.with_residual:
+                if x.shape[-1] == x_in.shape[-1]:
+                    x = x_in + x
+                else:
+                    assert x.shape[-1] % x_in.shape[-1] == 0
+                    x = x + x_in.repeat([*[1 for _ in x.shape[:-1]], x.shape[-1] // x_in.shape[-1]])
 
         return x

src/weathergen/model/model.py
@@ -13,11 +13,9 @@ import warnings
 from pathlib import Path
 
 import astropy_healpix as hp
-import astropy_healpix.healpy
 import numpy as np
 import torch
 from astropy_healpix import healpy
-from deepspeed.runtime.activation_checkpointing.checkpointing import checkpoint
 
 from weathergen.model.engines import (
     EmbeddingEngine,
@@ -32,19 +30,6 @@ from weathergen.model.layers import MLP
 from weathergen.model.utils import get_num_parameters
 from weathergen.utils.config import get_dtype
 from weathergen.utils.logger import logger
-
-
-def run_checkpointed(module, *args):
-    # This is the key: cast inputs to the same dtype as the module's weights.
-    casted_args = [
-        (
-            arg.to(module.weight.dtype)
-            if isinstance(arg, torch.Tensor) and arg.is_floating_point()
-            else arg
-        )
-        for arg in args
-    ]
-    return checkpoint(module, *casted_args)
 
 
 class ModelParams(torch.nn.Module):
@@ -535,11 +520,10 @@ class Model(torch.nn.Module):
                 continue
 
             for block in self.ae_local_blocks:
-                tokens_c = checkpoint(block, tokens_c, cell_lens_c)
+                tokens_c = block(tokens_c, cell_lens_c)
 
             for block in self.ae_adapter:
-                tokens_global_c = checkpoint(
-                    block,
+                tokens_global_c = block(
                     tokens_global_c,
                     tokens_c,
                     q_cells_lens_c,
@@ -562,7 +546,7 @@ class Model(torch.nn.Module):
     def assimilate_global(self, model_params, tokens):
         # global assimilation engine and adapter
         for block in self.ae_global_blocks:
-            tokens = checkpoint(block, tokens)
+            tokens = block(tokens)
 
         return tokens
 
@@ -570,7 +554,7 @@ class Model(torch.nn.Module):
     def forecast(self, model_params, tokens):
         for it, block in enumerate(self.fe_blocks):
             aux_info = torch.tensor([it], dtype=torch.float32, device="cuda")
-            tokens = checkpoint(block, tokens, aux_info)
+            tokens = block(tokens, aux_info)
 
         return tokens
 
@@ -599,7 +583,7 @@ class Model(torch.nn.Module):
                 chunks = []
                 for i_b in range(len(streams_data)):
                     if len(streams_data[i_b][ii].target_coords[fstep].shape) > 1:
-                        chunks += [checkpoint(tc_embed, streams_data[i_b][ii].target_coords[fstep])]
+                        chunks += [tc_embed(streams_data[i_b][ii].target_coords[fstep])]
                     else:
                         chunks += [streams_data[i_b][ii].target_coords[fstep]]
 
@@ -633,10 +617,9 @@ class Model(torch.nn.Module):
                 # apply prediction engine
                 for ib, block in enumerate(tte):
                     if self.cf.pred_self_attention and ib % 3 == 1:
-                        tc_tokens = checkpoint(block, tc_tokens, tcs_lens, tcs_aux)
+                        tc_tokens = block(tc_tokens, tcs_lens, tcs_aux)
                     else:
-                        tc_tokens = checkpoint(
-                            block,
+                        tc_tokens = block(
                             tc_tokens,
                             tokens_stream,
                             tcs_lens,
@@ -645,6 +628,6 @@ class Model(torch.nn.Module):
                         )
 
             # final prediction head to map back to physical space
-            preds_tokens += [checkpoint(self.pred_heads[ii], tc_tokens)]
+            preds_tokens += [self.pred_heads[ii](tc_tokens)]
 
         return preds_tokens

src/weathergen/model/norms.py
@@ -55,7 +55,8 @@ class RMSNorm(torch.nn.Module):
             torch.Tensor: The output tensor after applying RMSNorm.
 
         """
-        output = self._norm(x.float()).type_as(x)
+        with torch.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
+            output = self._norm(x.float()).type_as(x)
         return output * self.weight
 
 
@@ -78,10 +79,11 @@ class AdaLayerNorm(torch.nn.Module):
         self.norm = torch.nn.LayerNorm(dim_embed_x, norm_eps, norm_elementwise_affine)
 
     def forward(self, x: torch.Tensor, aux: torch.Tensor | None = None) -> torch.Tensor:
-        for block in self.embed_aux:
-            aux = block(aux)
-        scale, shift = aux.split(aux.shape[-1] // 2, dim=-1)
+        with torch.autocast(device_type="cuda", enabled=True, dtype=torch.bfloat16):
+            for block in self.embed_aux:
+                aux = block(aux)
+            scale, shift = aux.split(aux.shape[-1] // 2, dim=-1)
 
-        x = self.norm(x) * (1 + scale) + shift
+            x = self.norm(x) * (1 + scale) + shift
 
         return x

src/weathergen/train/trainer.py
@@ -19,7 +19,9 @@ from torch.distributed.fsdp import FullStateDictConfig, StateDictType
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp.fully_sharded_data_parallel import MixedPrecision, ShardingStrategy
 from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy  # default_auto_wrap_policy,
+from deepspeed.utils import set_z3_leaf_modules
 
+from weathergen.model.engines import EnsPredictionHead
 import weathergen.train.loss as losses
 import weathergen.utils.config as config
 from weathergen.datasets.multi_stream_data_sampler import MultiStreamDataSampler
@@ -441,8 +443,6 @@ class Trainer(Trainer_Base):
                 if target.shape[0] > 0 and pred.shape[0] > 0:
                     # extract data/coords and remove token dimension if it exists
                     pred = pred.reshape([pred.shape[0], *target.shape])
-                    _logger.debug(f"predition dtype: {pred.dtype}")
-                    _logger.debug(f"target dtype: {target.dtype}")
 
                     assert pred.shape[1] > 0
 