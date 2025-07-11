5faef098494ebf0b2b9bdb4aadf31313b17644e0
Michael <m.langguth@fz-juelich.de>
2025-07-11 15:57:39 +0200

    Add helper function to get custom last activation.

--- src/weathergen/model/utils.py
+++ src/weathergen/model/utils.py
@@ -20,3 +20,40 @@ def get_num_parameters(block):
 def freeze_weights(block):
     for p in block.parameters():
         p.requires_grad = False
+
+#########################################
+def get_activation(name: str):
+    """
+    Returns a PyTorch activation function based on the provided name.
+    Args:
+        name (str): The name of the activation function.
+    Returns:
+        torch.nn.Module: The corresponding activation function.
+    """
+    name = name.lower()
+    if name == "tanh":
+        return torch.nn.Tanh()
+    elif name == "softmax":
+        return torch.nn.Softmax(dim=-1)
+    elif name == "sigmoid":
+        return torch.nn.Sigmoid()
+    elif name == "gelu":
+        return torch.nn.GELU()
+    elif name == "identity":
+        return torch.nn.Identity()
+    elif name == "relu":
+        return torch.nn.ReLU()
+    elif name == "leakyrelu":
+        return torch.nn.LeakyReLU()
+    elif name == "elu":
+        return torch.nn.ELU()
+    elif name == "selu":
+        return torch.nn.SELU()
+    elif name == "prelu":
+        return torch.nn.PReLU()
+    elif name == "softplus":
+        return torch.nn.Softplus()
+    elif name == "linear":
+        return None
+    else:
+        raise ValueError(f"Unsupported activation type: {name}")
