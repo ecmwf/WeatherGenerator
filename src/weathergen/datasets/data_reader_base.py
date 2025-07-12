7bba0865d3c1ee8c548e4caf1945eb76f7d46d5e
jpolz <julius.polz@kit.edu>
2025-07-12 23:29:16 +0200

    ruff

--- src/weathergen/datasets/data_reader_base.py
+++ src/weathergen/datasets/data_reader_base.py
@@ -8,12 +8,12 @@
 # nor does it submit to any jurisdiction.
 
 import datetime
-import pandas as pd
 import logging
 from abc import abstractmethod
 from dataclasses import dataclass
 
 import numpy as np
+import pandas as pd
 from numpy import datetime64, timedelta64
 from numpy.typing import NDArray
 
