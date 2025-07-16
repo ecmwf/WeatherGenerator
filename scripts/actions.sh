af39ff110d62663c8a0984336824f5737a2cc784
Tim Hunter <tim.hunter@ecmwf.int>
2025-07-16 10:01:43 +0200

    slash

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -39,6 +39,8 @@ case "$1" in
       export working_dir=$(cat $(../WeatherGenerator-private/hpc/platform-env.py hpc-config) | uvx yq .path_shared_working_dir)
       # Remove quotes
       export working_dir=$(echo "$working_dir" | sed 's/[\"\x27]//g')
+      # If there is a trailing slash, remove it
+      working_dir=${working_dir%/}
       # If the working directory does not exist, exit with an error
       if [ ! -d "$working_dir" ]; then
         echo "Working directory $working_dir does not exist. Please check the configuration."
