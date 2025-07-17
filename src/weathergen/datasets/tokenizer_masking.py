Fixed randomization problem with masking  (#510)
* Fixed randomization problem with masking (needs to be verified)

* Making sure the seed is ok

* Fixed problem with seed init.

* More improvements. But problem still seems to be there.

* Clean up of rng handling. Re-initalization is passed through to masker, which was the issue.

* - Fixed prime numbers
- Cleaned up unnecessary rng init and added further comments.

---------

Co-authored-by: clessig <christian.lessig@ecwmf.int>

src/weathergen/datasets/tokenizer_masking.py
@@ -32,7 +32,7 @@ from weathergen.utils.logger import init_loggers
 
 
 class TokenizerMasking:
-    def __init__(self, healpix_level: int, seed: int, masker: Masker):
+    def __init__(self, healpix_level: int, masker: Masker):
         ref = torch.tensor([1.0, 0.0, 0.0])
 
         self.hl_source = healpix_level
@@ -123,20 +123,7 @@ class TokenizerMasking:
             .to(torch.float32)
         )
 
-        # ensure that the seed is sufficiently large so that the div below does not lead to
-        # aliasing
-        self.rng_seed = seed if seed > 16384 else seed * 16384
-        self._reinit_rng()
-
         self.size_time_embedding = 6
-
-    def _reinit_rng(self) -> None:
-        """
-        Reinitialize rng
-        """
-        worker_info = torch.utils.data.get_worker_info()
-        div_factor = (worker_info.id + 1) if worker_info is not None else 1
-        self.rng = np.random.default_rng(int(self.rng_seed / div_factor))
 
     def get_size_time_embedding(self) -> int:
         """
@@ -144,12 +131,12 @@ class TokenizerMasking:
         """
         return self.size_time_embedding
 
-    def reset(self) -> None:
+    def reset_rng(self, rng) -> None:
         """
-        Reset state after epoch
+        Reset rng after epoch to ensure proper randomization
         """
-        self.rng_seed *= 2
-        self._reinit_rng()
+        self.masker.reset_rng(rng)
+        self.rng = rng
 
     def batchify_source(
         self,