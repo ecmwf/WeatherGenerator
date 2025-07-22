6efd20cdf2a5907c4ccdb39e68acc1584e8572b2
Patnala,Ankit <a.patnala@fz-juelich.de>
2025-07-22 14:40:30 +0200

    remove overwriting of cf.streams from path for inference and train_continue

--- src/weathergen/run_train.py
+++ src/weathergen/run_train.py
@@ -61,9 +61,7 @@ def inference_from_args(argl: list[str]):
     cf = config.set_run_id(cf, args.run_id, args.reuse_run_id)
 
     cf.run_history += [(args.from_run_id, cf.istep)]
-    cf.streams = config.load_streams(Path(cf.streams_directory))
     cf = config.set_paths(cf)
-
     trainer = Trainer()
     trainer.inference(cf, args.from_run_id, args.epoch)
 
@@ -115,7 +113,6 @@ def train_continue() -> None:
 
     # track history of run to ensure traceability of results
     cf.run_history += [(args.from_run_id, cf.istep)]
-    cf.streams = config.load_streams(Path(cf.streams_directory))
     cf = config.set_paths(cf)
 
     if args.finetune_forecast:
