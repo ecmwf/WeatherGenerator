9daf5c103256bdece553345cfa384cb1bd8fe435
Timothy Hunter <tim.hunter@ecmwf.int>
2025-07-22 08:58:37 +0000

    changes

--- packages/evaluate/src/weathergen/evaluate/plot.py
+++ packages/evaluate/src/weathergen/evaluate/plot.py
@@ -8,6 +8,10 @@
 # ///
 
 
+import logging
+
+import plotly.express as px
+
 from weathergen.common import common_function
 
 
@@ -16,4 +20,9 @@ def plot():
 
 
 if __name__ == "__main__":
+    # Set up logging to display debug messages
+    logging.basicConfig(level=logging.DEBUG)
     plot()
+    data_canada = px.data.gapminder().query("country == 'Canada'")
+    fig = px.bar(data_canada, x="year", y="pop")
+    fig.write_image("fig1.png")
