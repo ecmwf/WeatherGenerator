42a73cdd2655f1e532d9a5aca61814fb54781013
Christian Lessig <christian.lessig@ecmwf.int>
2025-07-31 08:26:24 +0200

    Adding assert to catch inconsistent config params (#630)

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -56,6 +56,7 @@ class Trainer(TrainerBase):
 
         assert cf.samples_per_epoch % cf.batch_size_per_gpu == 0
         assert cf.samples_per_validation % cf.batch_size_validation_per_gpu == 0
+        assert cf.forecast_policy if cf.forecast_steps > 0 else True
 
         self.mixed_precision_dtype = get_dtype(cf.attention_dtype)
 
