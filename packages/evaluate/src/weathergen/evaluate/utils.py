fe5edcd16108e23f05d25cf3e86982b5d237e46e
Tim Hunter <tim.hunter@ecmwf.int>
2025-08-01 15:37:07 +0200

    changes

--- packages/evaluate/src/weathergen/evaluate/utils.py
+++ packages/evaluate/src/weathergen/evaluate/utils.py
@@ -15,9 +15,10 @@ from pathlib import Path
 import numpy as np
 import omegaconf as oc
 import xarray as xr
-from plotter import DefaultMarkerSize, LinePlots, Plotter
-from score import VerifiedData, get_score
 from tqdm import tqdm
+
+from weathergen.evaluate.plotter import DefaultMarkerSize, LinePlots, Plotter
+from weathergen.evaluate.score import VerifiedData, get_score
 
 from weathergen.common.io import ZarrIO
 from weathergen.evaluate.score_utils import to_list
