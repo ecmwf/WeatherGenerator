e2cc583ec8d35cdf067bc91210d17b2994eb2561
Timothy Hunter <tim.hunter@ecmwf.int>
2025-08-05 13:54:56 +0200

    [595] Changes for running a notebook script  (#598)
    * Changes
    
    * Chanegs
    
    * work
    
    * change
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * reverse old changes
    
    * linter

--- packages/evaluate/src/weathergen/evaluate/utils.py
+++ packages/evaluate/src/weathergen/evaluate/utils.py
@@ -15,11 +15,11 @@ from pathlib import Path
 import numpy as np
 import omegaconf as oc
 import xarray as xr
-from plotter import DefaultMarkerSize, LinePlots, Plotter
-from score import VerifiedData, get_score
 from tqdm import tqdm
 
 from weathergen.common.io import ZarrIO
+from weathergen.evaluate.plotter import DefaultMarkerSize, LinePlots, Plotter
+from weathergen.evaluate.score import VerifiedData, get_score
 from weathergen.evaluate.score_utils import to_list
 
 _logger = logging.getLogger(__name__)
@@ -310,9 +310,7 @@ def plot_data(cfg: str, run_id: str, stream: str, stream_dict: dict) -> list[str
     # Check if histograms should be plotted
     plot_histograms = plot_settings.get("plot_histograms", False)
     if not isinstance(plot_settings.plot_histograms, bool):
-        raise TypeError(
-            "plot_histograms must be a boolean."
-        )
+        raise TypeError("plot_histograms must be a boolean.")
 
     if plot_fsteps == "all":
         plot_fsteps = None
