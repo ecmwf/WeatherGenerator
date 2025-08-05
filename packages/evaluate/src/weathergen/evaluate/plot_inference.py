5390ed843e5f565ea81a2945576c55f20a3f579f
Timothy Hunter <tim.hunter@ecmwf.int>
2025-08-05 13:54:56 +0200

    [595] Changes for running a notebook script  (#598)
    * Changes
    
    * Chanegs
    
    * work
    
    * change
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * reverse old changes
    
    * linter

--- packages/evaluate/src/weathergen/evaluate/plot_inference.py
+++ packages/evaluate/src/weathergen/evaluate/plot_inference.py
@@ -13,9 +13,9 @@ import logging
 from collections import defaultdict
 from pathlib import Path
 
-from omegaconf import OmegaConf
-from plotter import Plotter
-from utils import (
+from omegaconf import DictConfig, OmegaConf
+
+from weathergen.evaluate.utils import (
     calc_scores_per_stream,
     metric_list_to_json,
     plot_data,
@@ -25,23 +25,8 @@ from utils import (
 
 _logger = logging.getLogger(__name__)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Fast evaluation of WeatherGenerator runs."
-    )
-    parser.add_argument(
-        "--config",
-        type=str,
-        help="Path to the configuration yaml file for plotting. e.g. config/plottig_config.yaml",
-    )
 
-    args = parser.parse_args()
-
-    # configure logging
-    logging.basicConfig(level=logging.INFO)
-
-    # load configuration
-    cfg = OmegaConf.load(args.config)
+def run_main(cfg: DictConfig) -> None:
     runs = cfg.run_ids
 
     _logger.info(f"Detected {len(runs)} runs")
@@ -52,14 +37,12 @@ if __name__ == "__main__":
     out_scores_dir = Path(cfg.output_scores_dir)
     out_scores_dir.mkdir(parents=True, exist_ok=True)
 
-    results_dir = Path(cfg.results_dir)
     metrics = cfg.evaluation.metrics
 
     # to get a structure like: scores_dict[metric][stream][run_id] = plot
     scores_dict = defaultdict(lambda: defaultdict(dict))
 
     for run_id, run in runs.items():
-        plotter = Plotter(cfg, run_id)
         _logger.info(f"RUN {run_id}: Getting data...")
 
         streams = run["streams"].keys()
@@ -71,7 +54,7 @@ if __name__ == "__main__":
 
             if stream_dict.get("plotting"):
                 _logger.info(f"RUN {run_id}: Plotting stream {stream}...")
-                plots = plot_data(cfg, run_id, stream, stream_dict)
+                plot_data(cfg, run_id, stream, stream_dict)
 
             if stream_dict.get("evaluation"):
                 _logger.info(f"Retrieve or compute scores for {run_id} - {stream}...")
@@ -109,9 +92,27 @@ if __name__ == "__main__":
                         scores_dict[metric][stream][run_id] = all_metrics.sel(
                             {"metric": metric}
                         )
+    # plot summary
+    if scores_dict and cfg.summary_plots:
+        _logger.info("Started creating summary plots..")
+        plot_summary(cfg, scores_dict, print_summary=cfg.print_summary)
 
 
-# plot summary
-if scores_dict and cfg.summary_plots:
-    _logger.info("Started creating summary plots..")
-    plot_summary(cfg, scores_dict, print_summary=cfg.print_summary)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Fast evaluation of WeatherGenerator runs."
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        help="Path to the configuration yaml file for plotting. e.g. config/plottig_config.yaml",
+    )
+
+    args = parser.parse_args()
+
+    # configure logging
+    logging.basicConfig(level=logging.INFO)
+
+    # load configuration
+    cfg = OmegaConf.load(args.config)
+    run_main(cfg)
