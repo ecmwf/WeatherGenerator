3b00b83e6b1ceee14afc97b69e5044e837b9c1d8
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-08-12 14:09:46 +0200

    lint

--- src/weathergen/utils/config.py
+++ src/weathergen/utils/config.py
@@ -68,11 +68,16 @@ def load_model_config(run_id: str, epoch: int | None, model_path: str | None) ->
         _logger.info(f"Loading config from provided full run_id path: {fname}")
     else:
         # Load private config here...
-        shared_model_path = Path(_load_private_conf(
-            private_home=Path(model_path) if model_path else None
-        ).get("path_shared_working_dir")) / "models"
+        shared_model_path = (
+            Path(
+                _load_private_conf(private_home=Path(model_path) if model_path else None).get(
+                    "path_shared_working_dir"
+                )
+            )
+            / "models"
+        )
         fname = shared_model_path / run_id / _get_model_config_file_name(run_id, epoch)
-        
+
     _logger.info(f"Loading config from specified run_id and epoch: {fname}")
 
     with fname.open() as f:
