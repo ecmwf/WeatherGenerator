b3ed5a21e674bd4ffeb800cc302b1c7f1c52343a
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-08-20 16:33:27 +0200

    intermediate commit

--- packages/evaluate/src/weathergen/evaluate/run_evaluation.py
+++ packages/evaluate/src/weathergen/evaluate/run_evaluation.py
@@ -13,7 +13,7 @@ import logging
 import sys
 from collections import defaultdict
 from pathlib import Path
-
+from weathergen.common.io import ZarrIO
 from omegaconf import OmegaConf
 
 from weathergen.evaluate.utils import (
@@ -22,6 +22,7 @@ from weathergen.evaluate.utils import (
     plot_data,
     plot_summary,
     retrieve_metric_from_json,
+    peek_tar_channels
 )
 from weathergen.utils.config import _REPO_ROOT, load_config, set_paths
 
@@ -130,17 +131,24 @@ def evaluate_from_args(argl: list[str]) -> None:
                             metric,
                             run.epoch,
                         )
-                        try:
-                            if not _check_metric(metric, metric_data, stream_dict):
-                                _logger.info("check failed")
-                                metrics_to_compute.append(metric)
-                            else:
-                                _logger.info("check passed")
-                                scores_dict[metric][region][stream][run_id] = (
-                                    metric_data
-                                )
-                        except (FileNotFoundError, KeyError, ValueError):
+                        # try:
+                        checked, (channels, fsteps, samples) =  _check_metric(metric, stream, run, metric_data, stream_dict, results_dir)
+                        if not checked:
                             metrics_to_compute.append(metric)
+                        else:
+                            #simply select the chosen eval channels, samples, fsteps here...
+                            # for sample in samples:
+                            scores_dict[metric][region][stream][run_id] = (
+                                metric_data.sel(channel=list(channels), forecast_step=list(fsteps))
+                            )
+                                # scores_dict[metric][region][stream][run_id][sample] = (
+                                #     metric_data.sel(channel=list(channels), forecast_step=list(fsteps))
+                                # )
+                        _logger.info(samples)
+                        _logger.info(metric_data)
+                        # except (FileNotFoundError, KeyError, ValueError):
+                        #     metrics_to_compute.append(metric)
+                    
 
                     if metrics_to_compute:
                         all_metrics, points_per_sample = calc_scores_per_stream(
@@ -157,56 +165,116 @@ def evaluate_from_args(argl: list[str]) -> None:
                             run.epoch,
                         )
 
+                        _logger.info('recomputed...')
+                        _logger.info(all_metrics)
+
                     for metric in metrics_to_compute:
                         scores_dict[metric][region][stream][run_id] = all_metrics.sel(
                             {"metric": metric}
                         )
+
     # plot summary
 
     if scores_dict and cfg.summary_plots:
         _logger.info("Started creating summary plots..")
         plot_summary(cfg, scores_dict, summary_dir, print_summary=cfg.print_summary)
 
+def _check_metric(metric: str, stream: str, run: str, metric_data: dict, stream_dict: dict, results_dir: Path):
 
-def _check_metric(metric: str, metric_data: dict, stream_dict: dict):
-
-    channels_config = stream_dict.get("channels")
-    if channels_config is None:
-        _logger.info(
-                f"No channels specified for {metric}. Recomputing."
-            )
-        return False
-    else:
-        #check channels
-        channels_data = metric_data["channel"].values
-        missing_config_channels, missing_data_channels = [], []
-        for ch in channels_config:
-            if ch not in channels_data:
-                missing_config_channels.append(ch)
-        for ch in channels_data:
-            if ch not in channels_config:
-                missing_data_channels.append(ch)
-        if missing_config_channels or missing_data_channels:
-            _logger.info(
-                "Channels different from previous config. Recomputing if channels are available in the Zarr file."
-            )
-            return False
-        #check forecast steps
-        elif sorted(
-            [
-                int(fstep)
-                for fstep in stream_dict["evaluation"].get(
+    channels = stream_dict.get("channels")
+    fsteps = stream_dict["evaluation"].get(
                     "forecast_step"
                 )
-            ]
-        ) != sorted(metric_data["forecast_step"].values):
-            _logger.info(
-                "Forecast steps different from previous config. Recomputing for forecast steps available in the Zarr file."
-            )
-            return False
-        else:
-            return True
+    samples = stream_dict["evaluation"].get(
+                    "sample"
+                )
+    _logger.info(samples)
+    channels = None if channels == "all" or channels is None else set(channels)
+    fsteps = None if fsteps == "all" or fsteps is None else set(fsteps)
+    samples = None if samples == "all" or samples is None else set(samples)
+    
+    available_channels =  set(metric_data["channel"].values.ravel())
+    available_fsteps = set(metric_data["forecast_step"].values.ravel())
+    available_samples = set(metric_data.coords["sample"].values.ravel())
 
+    fname_zarr = results_dir.joinpath(
+        f"validation_epoch{run['epoch']:05d}_rank{run['rank']:04d}.zarr"
+    )
+
+    if not fname_zarr.exists() or not fname_zarr.is_dir():
+        _logger.error(f"Zarr file {fname_zarr} does not exist or is not a directory.")
+        raise FileNotFoundError(
+            f"Zarr file {fname_zarr} does not exist or is not a directory."
+        )
+
+    with ZarrIO(fname_zarr) as zio:
+        zio_fsteps =  zio.forecast_steps
+        zio_samples = zio.samples
+        zio_channels = peek_tar_channels(zio, stream, zio_fsteps[0])
+        zio_fsteps = set([int(fstep) for fstep in zio_fsteps])
+        zio_samples = set([int(sample) for sample in zio_samples])
+        zio_channels = set(zio_channels)
+
+    _logger.info(
+    f"Requested: \n"
+    f"fsteps: {fsteps} \n"
+    f"samples: {samples} \n"
+    f"channels: {channels}"
+    )
+
+    _logger.info(
+    f"Available: \n"
+    f"available_fsteps: {available_fsteps} \n"
+    f"available_samples: {available_samples} \n"
+    f"available_channels: {available_channels}"
+    )
+
+    _logger.info(
+    f"Zarr: \n"
+    f"zio_fsteps: {zio_fsteps} \n"
+    f"zio_samples: {zio_samples} \n"
+    f"zio_channels: {zio_channels}"
+    )
+
+    #available must equal zio, otherwise recompute
+    if channels is None:
+        channels = available_channels
+        if zio_channels != available_channels:
+            _logger.info('Requested all channels, but previous config was a strict subset of channels in Zarr. Recomputing.')
+            return False, (channels, fsteps, samples)
+    if fsteps is None:
+        fsteps = available_fsteps
+        if zio_fsteps != available_fsteps:
+            _logger.info('Requested all fsteps, but previous config was a strict subset of fsteps in Zarr. Recomputing.')
+            return False, (channels, fsteps, samples)
+    if samples is None:
+        samples = available_samples
+        if zio_samples != available_samples:
+            _logger.info('Requested all samples, but previous config was a strict subset of samples in Zarr. Recomputing.')
+            return False, (channels, fsteps, samples)
+
+    #config must be a subset of zio, otherwise error
+    if not channels <= zio_channels:
+        raise ValueError(f'Requested channels that do not exist in the Zarr file. Channels must be a subset of {zio_channels}')
+    if not fsteps <= zio_fsteps:
+        raise ValueError(f'Requested fsteps that do not exist in the Zarr file. Fsteps must be a subset of {zio_fsteps}')
+    if not samples <= zio_samples:
+        raise ValueError(f'Requested samples that do not exist in the Zarr file. Samples must be a subset of {zio_samples}')
+
+    #config must be subset of available, otherwise recompute
+    if not channels <= available_channels:
+        _logger.info(f'Channel(s) {channels - available_channels} are not a availble from previous evaluation. Recomputing.')
+        return False, (channels, fsteps, samples)
+    if not fsteps <= available_fsteps:
+        _logger.info(f'Fstep(s) {fsteps - available_fsteps} are not a availble from previous evaluation. Recomputing.')
+        return False, (channels, fsteps, samples)
+    if not samples <= available_samples:
+        _logger.info(f'Sample(s) {samples - available_samples} are not a availble from previous evaluation. Recomputing.')
+        return False, (channels, fsteps, samples) 
+
+    #return True if all checks pass – i.e. no need to recompute
+    _logger.info("All checks passed – No need to recompute...")
+    return True, (channels, fsteps, samples)
 
 if __name__ == "__main__":
     evaluate()
