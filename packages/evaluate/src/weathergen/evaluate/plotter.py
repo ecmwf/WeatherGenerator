675e020be53be86a5c0049add5f88e8df49bdef1
iluise <luise.ilaria@gmail.com>
2025-08-27 10:21:08 +0200

    fix plotting

--- packages/evaluate/src/weathergen/evaluate/plotter.py
+++ packages/evaluate/src/weathergen/evaluate/plotter.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import os
 from pathlib import Path
@@ -207,7 +208,9 @@ class Plotter:
 
             for (valid_time, targ_t), (_, prd_t) in groups:
                 if valid_time is not None:
-                    _logger.debug(f"Plotting map for {var} at valid_time {valid_time}")
+                    _logger.debug(
+                        f"Plotting histogram for {var} at valid_time {valid_time}"
+                    )
 
                 name = self.plot_histogram(targ_t, prd_t, hist_output_dir, var, tag=tag)
                 plot_names.append(name)
@@ -320,6 +323,22 @@ class Plotter:
         """
         self.update_data_selection(select)
 
+        map_kwargs_save = (
+            {
+                key: value
+                for key, value in map_kwargs.copy().items()
+                if key not in variables
+            }
+            if map_kwargs is not None
+            else {}
+        )
+
+        if not hasattr(map_kwargs_save, "marker_size"):
+            # Set default marker size if not specified in maps_config
+            map_kwargs_save["marker_size"] = DefaultMarkerSize.get_marker_size(
+                self.stream
+            )
+
         # Basic map output directory for this stream
         map_output_dir = self.get_map_output_dir(tag)
 
@@ -352,7 +371,7 @@ class Plotter:
                     map_output_dir,
                     var,
                     tag=tag,
-                    map_kwargs=map_kwargs,
+                    map_kwargs=map_kwargs[var],
                 )
                 plot_names.append(name)
 
@@ -393,6 +412,8 @@ class Plotter:
         marker_size_base = map_kwargs_save.pop("marker_size", 1)
         scale_marker_size = map_kwargs_save.pop("scale_marker_size", False)
         marker = map_kwargs_save.pop("marker", "o")
+        vmin = map_kwargs_save.pop("vmin", None)
+        vmax = map_kwargs_save.pop("vmax", None)
 
         # Create figure and axis objects
         fig = plt.figure(dpi=self.dpi_val)
@@ -401,7 +422,11 @@ class Plotter:
 
         marker_size = marker_size_base
         if scale_marker_size:
-            marker_size = (marker_size + 1.0) * np.cos(np.radians(data["lat"]))
+            marker_size = np.clip(
+                marker_size / np.cos(np.radians(data["lat"])) ** 2,
+                a_max=marker_size * 10.0,
+                a_min=marker_size,
+            )
 
         valid_time = str(data["valid_time"][0].values.astype("datetime64[m]"))
 
@@ -413,9 +438,12 @@ class Plotter:
             s=marker_size,
             marker=marker,
             transform=ccrs.PlateCarree(),
+            vmin=vmin,
+            vmax=vmax,
             linewidths=0.0,  # only markers, avoids aliasing for very small markers
             **map_kwargs_save,
         )
+
         plt.colorbar(
             scatter_plt, ax=ax, orientation="horizontal", label=f"Variable: {varname}"
         )
@@ -472,11 +500,26 @@ class Plotter:
 
         for _, sa in enumerate(samples):
             for _, var in enumerate(variables):
+                _logger.info(f"Creating animation for {var} sample: {sa} - {tag}")
                 image_paths = []
                 for _, fstep in enumerate(fsteps):
-                    image_paths.append(
-                        f"{map_output_dir}/map_{self.run_id}_{tag}_{sa}_{self.stream}_{var}_{fstep:03d}.png"
-                    )
+                    # TODO: refactor to avoid code duplication with scatter_plot
+                    parts = [
+                        "map",
+                        self.run_id,
+                        tag,
+                        str(sa),
+                        "*",
+                        self.stream,
+                        var,
+                        str(fstep).zfill(3),
+                    ]
+
+                    name = "_".join(filter(None, parts))
+                    fname = f"{map_output_dir.joinpath(name)}.{self.image_format}"
+
+                    names = glob.glob(fname)
+                    image_paths += names
 
                 images = [Image.open(path) for path in image_paths]
                 images[0].save(
