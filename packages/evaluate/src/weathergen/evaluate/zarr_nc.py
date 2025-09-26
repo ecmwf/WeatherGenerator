ac50fb4f9b6da56d6cd2931bd98e5775db66c1ad
Sorcha <sorcha.owens@metoffice.gov.uk>
2025-09-26 13:59:00 +0200

    refactoring, found issue with forecast_ref_time

--- packages/evaluate/src/weathergen/evaluate/zarr_nc.py
+++ packages/evaluate/src/weathergen/evaluate/zarr_nc.py
@@ -25,8 +25,12 @@ if not _logger.handlers:
     handler.setFormatter(formatter)
     _logger.addHandler(handler)
 
-
+## all functions
 def find_pl(all_variables):
+    """
+    Find all the pressure levels for each variable and return a dictionary
+    mapping variable names to their corresponding pressure levels.
+    """
     var_dict = {}
     pl = []
     for var in all_variables:
@@ -41,8 +45,12 @@ def find_pl(all_variables):
     pl = list(set(pl))
     return var_dict, pl
 
-
 def reshape_dataset(input_data_array):
+    """
+    Reshape the input xarray DataArray to have dimensions (ipoint, pressure_level)
+    for variables with multiple pressure levels, and (ipoint,) for surface variables.
+    Removes ipoint to valid_time, lat, lon after splitting for each sample
+    """
     var_dict, pl = find_pl(input_data_array.channel.values)
     data_vars = {}
     for new_var, old_vars in var_dict.items():
@@ -69,26 +77,30 @@ def reshape_dataset(input_data_array):
     sampled_data = [ds.assign_coords(sample=i) for i, ds in enumerate(sampled_data)]
     return sampled_data
 
-
 def remove_ipoint(sample_data):
+    """
+    Remove ipoint dimension by setting it as index and unstacking.
+    """
     sample_data = sample_data.set_index(ipoint=("valid_time", "lat", "lon")).unstack(
         "ipoint"
     )
     return sample_data
 
-
-def add_conventions(run_id, ds):
-    ds.attrs["title"] = f"WeatherGenerator Output for {run_id}"
+def add_conventions(stream, run_id, ds):
+    """
+    Add CF conventions to the dataset attributes.
+    """
+    ds.attrs["title"] = f"WeatherGenerator Output for {run_id} using stream {stream}"
     ds.attrs["institution"] = "WeatherGenerator Project"
     ds.attrs["source"] = "WeatherGenerator v0.0"
-    ds.attrs["history"] = "none"
+    ds.attrs["history"] = "Created using the zarr_nc.py script on " + np.datetime_as_string(np.datetime64("now"), unit="s")
     ds.attrs["Conventions"] = "CF-1.12"
     return ds
 
-
-
-
 def cf_parser(config, ds) -> xr.Dataset:
+    """
+    Parse the dataset according to the CF conventions specified in the config.
+    """
     # Start a new xarray dataset from scratch, it's easier than deleting / renaming (I tried!).
     variables = {}
     mapping = config["variables"]
@@ -154,33 +166,46 @@ def cf_parser(config, ds) -> xr.Dataset:
     dataset.attrs = ds.attrs
     return dataset
 
-
-def output_filename(type, run_id, sample_idx, output_dir, output_format):
+def output_filename(type, run_id, output_dir, output_format, forecast_ref_time):
+    """
+    Generate output filename based on type, run_id, sample index, output directory, format and forecast_ref_time.
+    """
     if output_format not in ["netcdf"]:
         raise ValueError(f"Unsupported output format: {output_format}")
     file_extension = "nc"
+    frt = np.datetime_as_string(forecast_ref_time, unit="h")
     # out_fname = Path(output_dir) / f'{type}_{run_id}_{np.datetime_as_string(forecast_ref_time, unit="h")}.{file_extension}'
+    # documentation wants <type>_<forecast_reference_time>_<forecast_period>_<collection>.<extension> 
+    #here multiple forecast steps are in one forecast so have amended to just use forecast reference time
     out_fname = (
-        Path(output_dir) / f"{type}_{run_id}_sample{sample_idx}.{file_extension}"
+        Path(output_dir) / f"{type}_{frt}_{run_id}.{file_extension}"
     )
     return out_fname
 
-
 def zarr_store(run_id):
+    """
+    Get the path to the Zarr store for a given run ID.
+    """
     run_results = (
         Path(_load_private_conf(None)["path_shared_working_dir"]) / f"results/{run_id}"
     )
     zarr_path = run_results / "validation_epoch00000_rank0000.zarr"
+    # TODO: this might need to be more flexible for epochs and ranks
     if not zarr_path.exists() or not zarr_path.is_dir():
         raise FileNotFoundError(
             f"Zarr file {zarr_path} does not exist or is not a directory."
         )
     return zarr_path
-
-
-def get_data1(run_id: str, stream: str, fsteps=None, channels=None):
+    
+def get_data(run_id: str, stream: str, type:str, fsteps=None, channels=None):
+    """
+    Retrieve data from Zarr store for a given run ID and stream.
+    type: 'target' or 'prediction'
+    """
+    # check type is valid
+    if type not in ['target', 'prediction']:
+        raise ValueError(f"Invalid type: {type}. Must be 'target' or 'prediction'.")
     fname_zarr = zarr_store(run_id)
-
     with ZarrIO(fname_zarr) as zio:
         zio_forecast_steps = zio.forecast_steps
         samples = zio.samples
@@ -191,39 +216,52 @@ def get_data1(run_id: str, stream: str, fsteps=None, channels=None):
         fsteps = zio_forecast_steps if fsteps is None else fsteps
         fsteps = sorted([int(fstep) for fstep in fsteps])
         samples = sorted([int(sample) for sample in samples])
-
-        da_tars, da_preds = [], []
-
-        for fstep in range(1, len(fsteps)):
-            da_tars_fs, da_preds_fs = [], []
-
-            for sample in tqdm(
-                samples,
-                desc=f"Processing {run_id} - stream: {stream} - forecast step: {fstep}",
-            ):
+    da_list = []
+    for fstep in range(1,  len(fsteps)):
+        da_fs = []
+        for sample in tqdm(
+            samples,
+            desc=f"Processing {run_id} - stream: {stream} - forecast step: {fstep}",
+        ):
+            with ZarrIO(fname_zarr) as zio:
                 out = zio.get_data(sample, stream, fstep)
-                target, pred = out.target.as_xarray(), out.prediction.as_xarray()
-                da_tars_fs.append(target.squeeze())
-                da_preds_fs.append(pred.squeeze())
+                if type == 'target':
+                    data = out.target.as_xarray()
+                elif type == 'prediction':
+                    data = out.prediction.as_xarray()
+            da_fs.append(data.squeeze())
+        da_fs = xr.concat(da_fs, dim="ipoint")
+        if set(channels) != set(all_channels):
+            available_channels = da_fs.channel.values
+            existing_channels = [ch for ch in channels if ch in available_channels]
+            if len(existing_channels) < len(channels):
+                _logger.info(
+                    f"The following channels were not found: {list(set(channels) - set(existing_channels))}. Skipping them."
+                )
+            da_fs = da_fs.sel(channel=existing_channels)
+        da_list.append(da_fs)
+    return da_list
 
-            da_tars_fs = xr.concat(da_tars_fs, dim="ipoint")
-            da_preds_fs = xr.concat(da_preds_fs, dim="ipoint")
 
-            if set(channels) != set(all_channels):
-                available_channels = da_tars_fs.channel.values
-                existing_channels = [ch for ch in channels if ch in available_channels]
-                if len(existing_channels) < len(channels):
-                    _logger.info(
-                        f"The following channels were not found: {list(set(channels) - set(existing_channels))}. Skipping them."
-                    )
-                da_tars_fs = da_tars_fs.sel(channel=existing_channels)
-                da_preds_fs = da_preds_fs.sel(channel=existing_channels)
-
-            da_tars.append(da_tars_fs)
-            da_preds.append(da_preds_fs)
-
-        return da_tars, da_preds
-
+def save_samples_to_netcdf(type_str, dict_sample_all_steps, FSTEP_HOURS, run_id, output_dir, output_format, config):
+    """
+    Uses dictionary of pred/target xarray DataArrays to save each sample to a NetCDF file.
+    """
+    for sample_idx, array_list in dict_sample_all_steps.items():
+        frt = array_list[0].coords['valid_time'].values[0] - FSTEP_HOURS
+        sample_all_steps = xr.concat(array_list, dim="forecast_step")
+        out_fname = output_filename(
+            type_str, run_id, output_dir, output_format, frt
+        )
+        _logger.info(f"Saving sample {sample_idx} to {out_fname}...")
+        sample_all_steps = sample_all_steps.assign_coords(
+            forecast_ref_time=frt
+        )
+        stream = str(sample_all_steps.coords['stream'].values)
+        sample_all_steps = sample_all_steps.drop_vars("sample")
+        sample_all_steps = cf_parser(config, sample_all_steps)
+        sample_all_steps = add_conventions(stream, run_id, sample_all_steps)
+        sample_all_steps.to_netcdf(out_fname, mode="w")
 
 def parse_args(args):
     """
@@ -263,14 +301,10 @@ def parse_args(args):
         choices=["ERA5"],
         help="Stream name to retrieve data for",
     )
-
     args, unknown_args = parser.parse_known_args(args)
-
     if unknown_args:
         _logger.warning(f"Unknown arguments: {unknown_args}")
-
     return args
-
 
 if __name__ == "__main__":
     # Get run_id zarr data as lists of xarray DataArrays
@@ -298,66 +332,27 @@ if __name__ == "__main__":
     # check config loaded correctly
     assert config["variables"]["q"] is not None
 
-    da_tars, da_preds = get_data1(run_id, stream)
+    FSTEP_HOURS = np.timedelta64(6, "h")
 
-    n_samples = len(np.unique(da_tars[0].sample))
-    fstep_hours = np.timedelta64(6, "h")
+    # TODO: add checks to see if netcdf has alreaby been made
+    # TODO: overwrite clobber = True
+    # pverwriting needs to be fixed
+    # PermissionError: [Errno 13] Permission denied: '/p/home/jusers/owens1/jureca/WeatherGen/test_output1/pred_grwnhykd_sample0.nc'
+    # ds.close() needed to avoid this?
+    # for now either wipe folder between runs or use new folder/don't insepct inbetween
 
-    valid_time = [
-        [np.unique(da.valid_time).squeeze() for da in da_pred] for da_pred in da_preds
-    ]
-    steps = [np.unique(da_pred[0].forecast_step.values) for da_pred in da_preds]
-    forecast_ref_time = [vt - fstep_hours for vt in valid_time[0]]
-
-    # Store predictions and targets per sample
-    pred_sample_all_steps = {}
-    targ_sample_all_steps = {}
-
-    for i, da_pred in enumerate(da_preds):
-        fs_i_all_sample_pred = reshape_dataset(da_pred)
-        for j in range(n_samples):
-            # TODO: check forecast step values and ordering
-            _logger.info(f"Processing sample {j}, forecast step {i + 1}")
-            pred_sample_all_steps.setdefault(j, []).append(fs_i_all_sample_pred[j])
-
-    for i, da_targ in enumerate(da_tars):
-        fs_i_all_sample_targ = reshape_dataset(da_targ)
-        for j in range(n_samples):
-            _logger.info(f"Processing sample {j}, forecast step {i + 1}")
-            targ_sample_all_steps.setdefault(j, []).append(fs_i_all_sample_targ[j])
-
-    # Save each sample's predictions
-    for sample_idx, pred_list in pred_sample_all_steps.items():
-        sample_all_steps = xr.concat(pred_list, dim="forecast_step")
-        # TODO: put forecast reference time in the filename
-        out_fname = output_filename(
-            "pred", run_id, sample_idx, output_dir, output_format
-        )
-        _logger.info(f"Saving sample {sample_idx} to {out_fname}...")
-        sample_all_steps = sample_all_steps.assign_coords(
-            forecast_ref_time=forecast_ref_time[sample_idx]
-        )
-        sample_all_steps = sample_all_steps.drop_vars("sample")
-        sample_all_steps = cf_parser(config, sample_all_steps)
-        sample_all_steps = add_conventions(run_id, sample_all_steps)
-        sample_all_steps.to_netcdf(out_fname, mode="w")
-
-    # Save each sample's targets
-    # TODO: filter useing data_type, make functions
-    for sample_idx, targ_list in targ_sample_all_steps.items():
-        sample_all_steps = xr.concat(targ_list, dim="forecast_step")
-        out_fname = output_filename(
-            "targ", run_id, sample_idx, output_dir, output_format
-        )
-        _logger.info(f"Saving sample {sample_idx} to {out_fname}...")
-        sample_all_steps = sample_all_steps.assign_coords(
-            forecast_ref_time=forecast_ref_time[sample_idx]
-        )
-        sample_all_steps = sample_all_steps.drop_vars("sample")
-        sample_all_steps = cf_parser(config, sample_all_steps)
-        sample_all_steps = add_conventions(run_id, sample_all_steps)
-        sample_all_steps.to_netcdf(out_fname, mode="w")
-        # pverwriting needs to be fixed
-        # PermissionError: [Errno 13] Permission denied: '/p/home/jusers/owens1/jureca/WeatherGen/test_output1/pred_grwnhykd_sample0.nc'
-        # ds.close() needed to avoid this?
-        # for now either wipe folder between runs or use new folder/don't insepct inbetween
+    for type in data_type:
+        _logger.info(f"Starting processing {type} for run ID {run_id}.")
+        da_list = get_data(run_id, stream, type)
+        n_samples = len(np.unique(da_list[0].sample))
+        dict_sample_all_steps = {}
+        for i, da_type in enumerate(da_list):
+            fs_i_all_sample = reshape_dataset(da_type)
+            for j in range(n_samples):
+                _logger.info(f"Processing sample {j}, forecast step {i + 1}")
+                dict_sample_all_steps.setdefault(j, []).append(fs_i_all_sample[j])
+        valid_time = [
+            [np.unique(da.valid_time).squeeze() for da in da_step] for da_step in da_list
+        ]
+        forecast_ref_times = [vt - FSTEP_HOURS for vt in valid_time[0]]
+        save_samples_to_netcdf(str(type)[:4], dict_sample_all_steps, FSTEP_HOURS, run_id, output_dir, output_format, config)
