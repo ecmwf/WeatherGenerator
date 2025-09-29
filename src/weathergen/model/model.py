5a38dc70c9a81dfc0dcbe4cce7e1f4f73030b965
Kerem Tezcan <kctezcan@gmail.com>
2025-09-29 11:45:18 +0200

    using !=

--- src/weathergen/model/model.py
+++ src/weathergen/model/model.py
@@ -666,7 +666,7 @@ class Model(torch.nn.Module):
                 for _, (s, embed) in enumerate(zip(sb, self.embeds, strict=False)):
                     for fstep in range(num_fsteps):
                         # TODO: KCT: should we actually remove the below check and just return an empty tensor?
-                        if not (s.target_srclk_tokens_lens[fstep].sum() == 0):
+                        if s.target_srclk_tokens_lens[fstep].sum() != 0:
                             idxs = s.target_srclk_idxs_embed[fstep]
                             idxs_pe = s.target_srclk_idxs_embed_pe[fstep]
 
