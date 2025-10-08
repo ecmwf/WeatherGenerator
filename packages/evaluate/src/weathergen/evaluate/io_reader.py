8786935f4cf49c5106cec7c52040ac361f7746cb
Matthias Karlbauer <matthias.karlbauer@ecmwf.int>
2025-10-08 16:11:49 +0000

    Interface improvements

--- packages/evaluate/src/weathergen/evaluate/io_reader.py
+++ packages/evaluate/src/weathergen/evaluate/io_reader.py
@@ -16,7 +16,7 @@ import omegaconf as oc
 import xarray as xr
 from tqdm import tqdm
 
-from weathergen.common.config import _load_private_conf, load_config, load_model_config
+from weathergen.common.config import get_shared_wg_path, load_config, load_model_config
 from weathergen.common.io import ZarrIO
 from weathergen.evaluate.derived_channels import DeriveChannels
 from weathergen.evaluate.score_utils import RegionBoundingBox, to_list
@@ -67,7 +67,9 @@ class DataAvailability:
 
 
 class Reader:
-    def __init__(self, eval_cfg: dict, run_id: str, private_paths: dict | None = None):
+    def __init__(
+        self, eval_cfg: dict, run_id: str, private_paths: dict[str, str] | None = None
+    ):
         """
         Generic data reader class.
 
@@ -77,7 +79,7 @@ class Reader:
             config with plotting and evaluation options for that run id
         run_id : str
             run id of the model
-        private_paths: dict
+        private_paths: dict[srt, str]
             dictionary of private paths for the supported HPC
         """
         self.eval_cfg = eval_cfg
@@ -283,10 +285,7 @@ class WeatherGenReader(Reader):
         self.inference_cfg = self.get_inference_config()
 
         if not self.results_base_dir:
-            pcfg = _load_private_conf()
-            self.results_base_dir = (
-                Path(pcfg.get("path_shared_working_dir")) / "results"
-            )
+            self.results_base_dir = Path(get_shared_wg_path("results"))
             _logger.info(
                 f"Results directory obtained from private config: {self.results_base_dir}"
             )
