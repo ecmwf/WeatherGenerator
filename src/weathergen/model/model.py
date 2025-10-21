cbb1c85e88fb8c2ec2d15b768bfedf12e95396e8
Jubeku <julian.kuehnert@ecmwf.int>
2025-10-21 17:33:40 +0200

    revert forecast fct, fix in separate PR

--- src/weathergen/model/model.py
+++ src/weathergen/model/model.py
@@ -596,7 +596,7 @@ class Model(torch.nn.Module):
                 if noise_std > 0.0:
                     tokens = tokens + torch.randn_like(tokens) * torch.norm(tokens) * noise_std
 
-            tokens = self.forecast(model_params, tokens, fstep)
+            tokens = self.forecast(model_params, tokens)
 
         # prediction for final step
         preds_all += [
@@ -793,7 +793,7 @@ class Model(torch.nn.Module):
         return tokens
 
     #########################################
-    def forecast(self, model_params: ModelParams, tokens: torch.Tensor, fstep: int) -> torch.Tensor:
+    def forecast(self, model_params: ModelParams, tokens: torch.Tensor) -> torch.Tensor:
         """Advances latent space representation in time
 
         Args:
@@ -806,7 +806,7 @@ class Model(torch.nn.Module):
         """
 
         for it, block in enumerate(self.fe_blocks):
-            aux_info = torch.tensor([fstep], dtype=torch.float32, device="cuda")
+            aux_info = torch.tensor([it], dtype=torch.float32, device="cuda")
             tokens = checkpoint(block, tokens, aux_info, use_reentrant=False)
 
         return tokens
