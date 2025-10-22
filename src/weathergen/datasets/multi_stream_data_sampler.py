Fix ordering issue for output points. This commit addresses the computation of the inverse index. Still missing is to use it. Also still needs to be tested and debugged.

src/weathergen/datasets/multi_stream_data_sampler.py
@@ -34,7 +34,7 @@ from weathergen.datasets.utils import (
     compute_source_cell_lens,
 )
 from weathergen.utils.distributed import is_root
-from weathergen.utils.train_logger import Stage
+from weathergen.utils.train_logger import TRAIN, Stage
 
 type AnyDataReader = DataReaderBase | DataReaderAnemoi | DataReaderObs
 
@@ -426,7 +426,7 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                             (time_win_target.start, time_win_target.end),
                         )
 
-                        stream_data.add_target(fstep, tt_cells, tc, tt_c, tt_t)
+                        stream_data.add_target(fstep, tt_cells, tc, tt_c, tt_t, idxs_inv)
 
                     # merge inputs for sources and targets for current stream
                     streams_data += [stream_data]

src/weathergen/datasets/stream_data.py
@@ -57,6 +57,8 @@ class StreamData:
         self.target_tokens_lens = [
             torch.tensor([0 for _ in range(self.healpix_cells)]) for _ in range(forecast_steps + 1)
         ]
+        # index to recover original ordering of data
+        self.target_idxs_inv = None
 
         # source tokens per cell
         self.source_tokens_cells = []
@@ -69,6 +71,8 @@ class StreamData:
         # processing after embedding
         self.source_idxs_embed = torch.tensor([])
         self.source_idxs_embed_pe = torch.tensor([])
+        # index to recover original ordering of data
+        self.source_idxs_inv = None
 
     def to_device(self, device: str) -> None:
         """
@@ -140,7 +144,12 @@ class StreamData:
         ]
 
     def add_source(
-        self, ss_raw: IOReaderData, ss_lens: torch.tensor, ss_cells: list, ss_centroids: list
+        self,
+        ss_raw: IOReaderData,
+        ss_lens: torch.tensor,
+        ss_cells: list,
+        ss_centroids: list,
+        idxs_inv: torch.tensor,
     ) -> None:
         """
         Add data for source for one input.
@@ -153,6 +162,7 @@ class StreamData:
             [ torch.tensor( tokens per cell, token size, number of channels) ]
         ss_centroids : list(number of healpix cells )
             [ torch.tensor( for source , 5) ]
+        idxs_inv : index to recover original ordering of datapoints
 
         Returns
         -------
@@ -163,6 +173,7 @@ class StreamData:
         self.source_tokens_lens = ss_lens
         self.source_tokens_cells = torch.cat(ss_cells)
         self.source_centroids = torch.cat(ss_centroids)
+        self.source_idxs_inv = idxs_inv
 
         idx = torch.isnan(self.source_tokens_cells)
         self.source_tokens_cells[idx] = self.mask_value
@@ -174,6 +185,7 @@ class StreamData:
         target_coords: torch.tensor,
         target_coords_raw: torch.tensor,
         times_raw: torch.tensor,
+        idxs_inv: torch.tensor,
     ) -> None:
         """
         Add data for target for one input.
@@ -193,6 +205,7 @@ class StreamData:
         target_times : list( number of healpix cells)
             [ torch.tensor( points per cell) ]
               absolute target times
+        idxs_inv : index to recover original ordering of datapoints
 
         Returns
         -------
@@ -202,7 +215,7 @@ class StreamData:
         self.target_tokens[fstep] = torch.cat(targets)
         self.target_coords[fstep] = torch.cat(target_coords)
         self.target_times_raw[fstep] = np.concatenate(times_raw)
-        self.target_coords_raw[fstep] = target_coords_raw
+        self.target_coords_raw[fstep] = torch.cat(target_coords_raw)
 
         tc = target_coords
         self.target_coords_lens[fstep] = torch.tensor(
@@ -213,6 +226,7 @@ class StreamData:
             [len(f) for f in targets] if len(targets) > 1 else self.target_tokens_lens[fstep],
             dtype=torch.int,
         )
+        self.target_idxs_inv = idxs_inv
 
     def target_empty(self) -> bool:
         """

src/weathergen/datasets/tokenizer_masking.py
@@ -70,7 +70,7 @@ class TokenizerMasking(Tokenizer):
             return (source_tokens_cells, source_tokens_lens, source_centroids)
 
         # tokenize all data first
-        tokenized_data = tokenize_window(
+        tokenized_data, idxs_inv = tokenize_window(
             0,
             rdata.coords,
             rdata.geoinfos,
@@ -83,8 +83,8 @@ class TokenizerMasking(Tokenizer):
         ]
 
         # Use the masker to get source tokens and the selection mask for the target
-        source_tokens_cells = self.masker.mask_source(
-            tokenized_data, rdata.coords, rdata.geoinfos, rdata.data
+        source_tokens_cells, idxs_inv = self.masker.mask_source(
+            tokenized_data, rdata.coords, rdata.geoinfos, rdata.data, idxs_inv
         )
 
         source_tokens_lens = torch.tensor([len(s) for s in source_tokens_cells], dtype=torch.int32)
@@ -93,7 +93,7 @@ class TokenizerMasking(Tokenizer):
         else:
             source_centroids = torch.tensor([])
 
-        return (source_tokens_cells, source_tokens_lens, source_centroids)
+        return (source_tokens_cells, source_tokens_lens, source_centroids, idxs_inv)
 
     def batchify_target(
         self,
@@ -131,7 +131,7 @@ class TokenizerMasking(Tokenizer):
         )
 
         # tokenize
-        target_tokens_cells = tokenize_window(
+        target_tokens_cells, idxs_inv = tokenize_window(
             0,
             rdata.coords,
             rdata.geoinfos,
@@ -139,8 +139,8 @@ class TokenizerMasking(Tokenizer):
             rdata.datetimes,
         )
 
-        target_tokens = self.masker.mask_target(
-            target_tokens_cells, rdata.coords, rdata.geoinfos, rdata.data
+        target_tokens, idxs_inv = self.masker.mask_target(
+            target_tokens_cells, rdata.coords, rdata.geoinfos, rdata.data, idxs_inv
         )
 
         target_tokens_lens = [len(t) for t in target_tokens]
@@ -214,7 +214,7 @@ class TokenizerMasking(Tokenizer):
             target_coords.requires_grad = False
             target_coords = list(target_coords.split(tt_lens))
 
-        return (target_tokens, target_coords, target_coords_raw, target_times_raw)
+        return (target_tokens, target_coords, target_coords_raw, target_times_raw, idxs_inv)
 
     def sample_tensors_uniform_vectorized(
         self, tensor_list: list, lengths: list, max_total_points: int

src/weathergen/datasets/tokenizer_utils.py
@@ -110,6 +110,7 @@ def hpy_cell_splits(coords: torch.tensor, hl: int):
 
     # extract information to split according to cells by first sorting and then finding split idxs
     hpy_idxs_ord = np.argsort(hpy_idxs, **numpy_argsort_args)
+    hpy_idxs_ord_inv = np.argsort(hpy_idxs_ord, **numpy_argsort_args)
     splits = np.flatnonzero(np.diff(hpy_idxs[hpy_idxs_ord]))
 
     # extract per cell data
@@ -119,7 +120,14 @@ def hpy_cell_splits(coords: torch.tensor, hl: int):
     for b, x in zip(np.unique(np.unique(hpy_idxs[hpy_idxs_ord])), hpy_idxs_ord_temp, strict=True):
         hpy_idxs_ord_split[b] = x
 
-    return (hpy_idxs_ord_split, thetas, phis, posr3)
+    hpy_idxs_ord_inv_temp = np.split(hpy_idxs_ord_inv, splits + 1)
+    hpy_idxs_ord_inv_split = [np.array([], dtype=np.int64) for _ in range(12 * 4**hl)]
+    for b, x in zip(
+        np.unique(np.unique(hpy_idxs[hpy_idxs_ord_inv])), hpy_idxs_ord_inv_temp, strict=True
+    ):
+        hpy_idxs_ord_inv_split[b] = x
+
+    return (hpy_idxs_ord_split, hpy_idxs_ord_inv_split, thetas, phis, posr3)
 
 
 def hpy_splits(
@@ -137,7 +145,7 @@ def hpy_splits(
     """
 
     # list of data points per healpix cell
-    (hpy_idxs_ord_split, thetas, phis, posr3) = hpy_cell_splits(coords, hl)
+    (hpy_idxs_ord_split, hpy_idxs_ord_inv_split, thetas, phis, posr3) = hpy_cell_splits(coords, hl)
 
     # if token_size is exceeed split based on latitude
     # TODO: split by hierarchically traversing healpix scheme
@@ -161,11 +169,16 @@ def hpy_splits(
         for idxs, ts, r in zip(hpy_idxs_ord_split, thetas_sorted, rem, strict=True)
     ]
 
+    idxs_ord_inv = [
+        list(torch.split(torch.from_numpy(idxs), token_size))
+        for idxs, ts, r in zip(hpy_idxs_ord_inv_split, thetas_sorted, rem, strict=True)
+    ]
+
     # extract length and flatten nested list
     idxs_ord_lens = [[len(a) for a in aa] for aa in idxs_ord]
     idxs_ord = [torch.cat([idxs for idxs in iidxs]) for iidxs in idxs_ord]
 
-    return idxs_ord, idxs_ord_lens, posr3
+    return idxs_ord, idxs_ord_lens, posr3, idxs_ord_inv
 
 
 def tokenize_window_space(
@@ -190,7 +203,7 @@ def tokenize_window_space(
         return
 
     # idx_ord_lens is length is number of tokens per healpix cell
-    idxs_ord, idxs_ord_lens, posr3 = hpy_splits(coords, hl, token_size, pad_tokens)
+    idxs_ord, idxs_ord_lens, posr3, idxs_ord_inv = hpy_splits(coords, hl, token_size, pad_tokens)
 
     # pad with zero at the beggining for token size padding
     times_enc = enc_time(times, time_win)
@@ -231,7 +244,7 @@ def tokenize_window_space(
         for i, (idxs, idxs_lens) in enumerate(zip(idxs_ord, idxs_ord_lens, strict=True))
     ]
 
-    return tokens_cells
+    return tokens_cells, idxs_ord_inv
 
 
 def tokenize_window_spacetime(
@@ -249,17 +262,19 @@ def tokenize_window_spacetime(
     pad_tokens=True,
     local_coords=True,
 ):
-    """Tokenize respecting an intrinsic time step in the data, i.e. each time step is tokenized
+    """
+    Tokenize respecting an intrinsic time step in the data, i.e. each time step is tokenized
     separately
     """
 
     num_healpix_cells = 12 * 4**hl
     tokens_cells = [[] for _ in range(num_healpix_cells)]
+    idxs_inv = [[] for _ in range(num_healpix_cells)]
 
     t_unique = np.unique(times)
     for _, t in enumerate(t_unique):
         mask = t == times
-        tokens_cells_cur = tokenize_window_space(
+        tokens_cells_cur, idxs_inv_cur = tokenize_window_space(
             stream_id,
             coords[mask],
             geoinfos[mask],
@@ -275,9 +290,11 @@ def tokenize_window_spacetime(
             local_coords,
         )
 
+        # merge tokens originating from different time steps
         tokens_cells = [t + tc for t, tc in zip(tokens_cells, tokens_cells_cur, strict=True)]
+        idxs_inv = [t + tc for t, tc in zip(idxs_inv, idxs_inv_cur, strict=True)]
 
-    return tokens_cells
+    return tokens_cells, idxs_inv
 
 
 def _coords_local(

src/weathergen/train/trainer.py
@@ -512,7 +512,7 @@ class Trainer(TrainerBase):
         targets_all = [[[] for _ in self.cf.streams] for _ in range(fsteps)]
         targets_lens = [[[] for _ in self.cf.streams] for _ in range(fsteps)]
 
-        # TODO: iterate over batches here in future, and change loop order to batch, stream, fstep
+        # TODO: iterate over batches, and change loop order to batch, stream, fstep
         for fstep in range(len(targets_rt)):
             for i_strm, target in enumerate(targets_rt[fstep]):
                 pred = preds[fstep][i_strm]
@@ -523,10 +523,6 @@ class Trainer(TrainerBase):
                 # extract data/coords and remove token dimension if it exists
                 pred = pred.reshape([pred.shape[0], *target.shape])
                 assert pred.shape[1] > 0
-
-                mask_nan = ~torch.isnan(target)
-                if pred[:, mask_nan].shape[1] == 0:
-                    continue
 
                 targets_lens[fstep][i_strm] += [target.shape[0]]
                 dn_data = self.dataset_val.denormalize_target_channels