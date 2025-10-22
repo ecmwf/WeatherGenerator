62ef8abb0b4fb0d09798a2ea65f27be0073a7a36
Tim Hunter <tim.hunter@ecmwf.int>
2025-10-22 17:27:42 +0200

    cleanup of type issues

--- packages/common/src/weathergen/common/config.py
+++ packages/common/src/weathergen/common/config.py
@@ -83,8 +83,8 @@ def load_model_config(run_id: str, epoch: int | None, model_path: str | None) ->
             model_path = _get_config_attribute(
                 config=pconf, attribute_name="model_path", fallback="models"
             )
-        model_path = Path(model_path)
-        fname = model_path / run_id / _get_model_config_file_name(run_id, epoch)
+        path = Path(model_path)
+        fname = path / run_id / _get_model_config_file_name(run_id, epoch)
         assert fname.exists(), (
             "The fallback path to the model does not exist. Please provide a `model_path`."
         )
@@ -195,7 +195,9 @@ def load_config(
     with open_dict(base_config):
         base_config.from_run_id = from_run_id
     # use OmegaConf.unsafe_merge if too slow
-    return OmegaConf.merge(base_config, private_config, *overwrite_configs)
+    c = OmegaConf.merge(base_config, private_config, *overwrite_configs)
+    assert isinstance(c, Config)
+    return c
 
 
 def _load_streams_in_config(config: Config) -> Config:
@@ -286,6 +288,7 @@ def _load_overwrite_conf(overwrite: Path | dict | DictConfig) -> DictConfig:
             msg = f"Cannot build config from overwrite: {overwrite}, with type {type(overwrite)}"
             raise ValueError(msg)
 
+    assert isinstance(overwrite_config, DictConfig)
     return overwrite_config
 
 
@@ -340,12 +343,15 @@ def _load_private_conf(private_home: Path | None = None) -> DictConfig:
     if "secrets" in private_cf:
         del private_cf["secrets"]
 
+    assert isinstance(private_cf, DictConfig)
     return private_cf
 
 
 def _load_default_conf() -> Config:
     """Deserialize default configuration."""
-    return OmegaConf.load(_DEFAULT_CONFIG_PTH)
+    c = OmegaConf.load(_DEFAULT_CONFIG_PTH)
+    assert isinstance(c, Config)
+    return c
 
 
 def load_streams(streams_directory: Path) -> list[Config]:
