7866ff7a18a900cb5bd5c653af11ea926221c2ad
Matthias Karlbauer <matthias.karlbauer@ecmwf.int>
2025-11-14 07:13:11 +0000

    Restructuring and correcting forward pass during inference

--- src/weathergen/model/diffusion.py
+++ src/weathergen/model/diffusion.py
@@ -34,13 +34,13 @@ class BatchData:
 
     def get_input_data(self, t: int):
         return self.model_samples[t]["data"]
-    
+
     def get_input_metadata(self, t: int):
         return self.model_samples[t]["metadata"]
 
     def get_target_data(self, t: int):
         return self.target_samples[t]["data"]
-    
+
     def get_target_metadata(self, t: int):
         return self.target_samples[t]["metadata"]
 
@@ -71,34 +71,43 @@ class DiffusionForecastEngine(torch.nn.Module):
         self.p_std = p_std
 
     def forward(self, data: BatchData) -> torch.Tensor:
+        """
+        Model forward call during training. Unpacks the conditioning c = [x_{t-k}, ..., x_{t}], the
+        target y = x_{t+1}, and the random noise eta from the data, computes the diffusion noise
+        level sigma, and feeds the noisy target along with the conditioning and sigma through the
+        model to return a denoised prediction.
+        """
         # Retrieve conditionings [0:-1], target [-1], and noise from data object.
-        # The data retrieval ignores batch and stream dimension for now (has to be adapted).
-        cond = [data.get_input_data(t) for t in range(data.get_sample_len() - 1)]
+        # TOOD: The data retrieval ignores batch and stream dimension for now (has to be adapted).
+        c = [data.get_input_data(t) for t in range(data.get_sample_len() - 1)]
         y = data.get_input_data(-1)
         eta = data.get_input_metadata(-1)
 
         # Compute sigma (noise level) from eta
-        #noise = torch.randn(y.shape, device=y.device)
+        # noise = torch.randn(y.shape, device=y.device)  # now eta from MultiStreamDataSampler
         sigma = (eta * self.p_std + self.p_mean).exp()
         n = torch.randn_like(y) * sigma
+        return self.denoise(x=y + n, c=c, sigma=sigma)
 
-        # Compute conditionings
+        # Compute loss -- move this to a separate loss calculator
+        # weight = (sigma**2 + self.sigma_data**2) / (sigma * self.sigma_data) ** 2  # Table 1
+        # loss = weight * ((y_hat - y) ** 2)
+
+    def denoise(self, x: torch.Tensor, c: torch.Tensor, sigma: float) -> torch.Tensor:
+        """
+        The actual diffusion step, where the model removes noise from the input x under
+        consideration of a conditioning c (e.g., previous time steps) and the current diffusion
+        noise level sigma.
+        """
+        # Compute scaling conditionings
         c_skip = self.sigma_data**2 / (sigma**2 + self.sigma_data**2)
         c_out = sigma * self.sigma_data / (sigma**2 + self.sigma_data**2).sqrt()
         c_in = 1 / (sigma**2 + self.sigma_data**2).sqrt
         c_noise = sigma.log() / 4
 
-        # Add noise, precondition input, and feed through network
-        x = y + n
-        x = self.preconditioner.precondition(x, cond)
-        net_out = self.net(c_in * x, c_noise)
-        y_hat = c_skip * y + c_out * net_out  # Eq. (7)
-
-        return y_hat
-
-        # Compute loss -- move this to a separate loss calculator
-        # weight = (sigma**2 + self.sigma_data**2) / (sigma * self.sigma_data) ** 2  # Table 1
-        # loss = weight * ((y_hat - y) ** 2)
+        # Precondition input and feed through network
+        x = self.preconditioner.precondition(x, c)
+        return c_skip * x + c_out * self.net(c_in * x, c_noise)  # Eq. (7) in EDM paper
 
     def inference(
         self,
@@ -127,7 +136,7 @@ class DiffusionForecastEngine(torch.nn.Module):
         ):  # 0, ..., N-1
             x_cur = x_next
 
-            # Increase noise temporarily. (Stochastic sampling?)
+            # Increase noise temporarily. (Stochastic sampling; not used for now)
             # gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
             # t_hat = self.net.round_sigma(t_cur + gamma * t_cur)
             # x_hat = x_cur + (t_hat**2 - t_cur**2).sqrt() * s_noise * torch.randn_like(x_cur)
@@ -135,7 +144,7 @@ class DiffusionForecastEngine(torch.nn.Module):
             t_hat = t_cur
 
             # Euler step.
-            denoised = self.net(x_hat, t_hat)
+            denoised = self.denoise(x=x_hat, c=None, sigma=t_hat)  # c to be discussed
             d_cur = (x_hat - denoised) / t_hat
             x_next = x_hat + (t_next - t_hat) * d_cur
 
