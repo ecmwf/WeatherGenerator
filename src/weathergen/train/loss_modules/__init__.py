c85c82a08976546badebadcf0423ee75ee304e9f
Jubeku <julian.kuehnert@ecmwf.int>
2025-11-21 13:59:46 +0100

    rename latent diff module in init

--- src/weathergen/train/loss_modules/__init__.py
+++ src/weathergen/train/loss_modules/__init__.py
@@ -1,4 +1,4 @@
-from .loss_module_latent import LossLatentDiffusion
+from .loss_module_latent_diffusion import LossLatentDiffusion
 from .loss_module_physical import LossPhysical, LossPhysicalTwo
 from .loss_module_ssl import LossStudentTeacher
 
