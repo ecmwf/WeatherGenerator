8a9873e390319bce00b51c153571c2d85816004f
simone99n <simone.norberti@gmail.com>
2025-12-04 11:13:32 +0100

    [450] multi stream test as default

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -94,14 +94,14 @@ case "$1" in
       uv run --no-project python scripts/check_tomls.py
     )
     ;;
-  integration-test)
+  integration-test-single)
     (
       cd "$SCRIPT_DIR" || exit 1
       uv sync --offline --all-packages --extra gpu
       uv run --offline pytest ./integration_tests/small1_test.py --verbose -s
     )
     ;;
-    integration-test-multi)
+    integration-test)
     (
       cd "$SCRIPT_DIR" || exit 1
       uv sync --offline --all-packages --extra gpu
