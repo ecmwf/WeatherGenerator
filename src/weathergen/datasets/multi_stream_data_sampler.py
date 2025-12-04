a67b5c34d6f455530a2f10d68ac650247c338bb3
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-12-04 13:29:40 +0100

    updated asserts

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -277,23 +277,19 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
         # native length of datasets, independent of mini_epoch length that has potentially been
         # specified
         forecast_len = (
-            self.len_hrs * (fsm)
+            self.len_hrs * (fsm + 1)
         ) // self.step_hrs  # TODO: check if it should be fsm + 1
         idx_end -= forecast_len + self.forecast_offset
 
         assert idx_end > 0, "dataset size too small for forecast range"
         self.perms = np.arange(index_range.start, idx_end)
-        if self.repeat_data and len(self.perms) < self.len:
-            assert self.samples_per_mini_epoch == self.len, (
-                "Length of sampler was set different from samples_per_mini_epoch –- aborting."
-            )
-            assert self.len % len(self.perms) == 0, (
-                "Length of permutations is not a multiple of length of available data –- aborting."
+        if self.repeat_data and len(self.perms) < self.samples_per_mini_epoch:
+            assert self.samples_per_mini_epoch % len(self.perms) == 0, (
+                "Length of mini epoch is not a multiple of length of available data –- aborting."
             )
             self.perms = np.tile(
-                self.perms, self.len // len(self.perms)
-            )  # TODO: maybe use samples_per_mini_epoch?
-            self.len = len(self.perms)
+                self.perms, self.samples_per_mini_epoch // len(self.perms)
+            )
 
         if self.shuffle:
             self.perms = self.rng.permutation(self.perms)
