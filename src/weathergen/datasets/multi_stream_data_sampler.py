c871f9c954c13f3d1f1d818139c929626115f954
moritzhauschulz <moritz.hauschulz@gmail.com>
2025-12-08 18:16:50 +0100

    remove unnecessary statement

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -172,8 +172,6 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                 ds = dataset(filename=filename, **kwargs)
 
                 fsm = self.forecast_steps[0]
-                if len(ds) > 0:
-                    self.len = min(self.len, len(ds) - (self.len_hrs * (fsm + 1)) // self.step_hrs)
 
                 # MODIFIES config !!!
                 stream_info[str(self._stage) + "_source_channels"] = ds.source_channels
