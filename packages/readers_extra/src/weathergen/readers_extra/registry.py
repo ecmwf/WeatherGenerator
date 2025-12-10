c30af6cbcb5ff8ec59eecc69b04c00f140c8424a
jpolz <julius.polz@kit.edu>
2025-12-10 04:09:32 +0100

    remove legacy code and local files

--- packages/readers_extra/src/weathergen/readers_extra/registry.py
+++ packages/readers_extra/src/weathergen/readers_extra/registry.py
@@ -16,10 +16,6 @@ def get_extra_reader(name: str, cf: Config) -> object | None:
     # There is no sanity check on them, so they may fail at runtime during imports
 
     match name:
-        case "icon":
-            from weathergen.readers_extra.data_reader_icon import DataReaderIcon
-
-            return ReaderEntry(cf.data_path_icon, DataReaderIcon)
         case "iconart":
             from weathergen.readers_extra.data_reader_iconart import DataReaderIconArt
 
