fix ctr_loss_fcts normalization; calculate per stream, per lfct average across channels and fsteps for logging

src/weathergen/train/loss_modules/loss_module_physical.py
@@ -207,6 +207,9 @@ class LossPhysical(LossModuleBase):
 
             stream_data = streams_data[i_batch][stream_name]
 
+            # get weigths for current streams
+            stream_loss_weight, weights_channels = self._get_weights(stream_info)
+
             fstep_loss_weights = self._get_fstep_weights(len(targets))
 
             loss_fsteps = torch.tensor(0.0, device=self.device, requires_grad=True)
@@ -232,9 +235,6 @@ class LossPhysical(LossModuleBase):
                 # expected final shape of pred is [ensemble_size, num_samples, num_channels].
                 pred = pred.reshape([pred.shape[0], *target.shape])
                 assert pred.shape[1] > 0
-
-                # get weigths for current streams
-                stream_loss_weight, weights_channels = self._get_weights(stream_info)
 
                 # get weights for locations
                 weights_locations = self._get_location_weights(
@@ -268,20 +268,45 @@ class LossPhysical(LossModuleBase):
 
                     # Add the weighted and normalized loss from this loss function to the total
                     # batch loss
-                    loss_fstep = loss_fstep + (
-                        loss_fct_weight * loss_lfct * stream_loss_weight * fstep_weight
-                    )
+                    loss_fstep = loss_fstep + (loss_fct_weight * loss_lfct * fstep_weight)
                     ctr_loss_fcts += 1 if loss_lfct > 0.0 else 0
 
-                loss_fsteps = loss_fsteps + (loss_fstep / ctr_loss_fcts if ctr_loss_fcts > 0 else 0)
+                loss_fsteps = loss_fsteps + loss_fstep
                 ctr_fsteps += 1 if ctr_loss_fcts > 0 else 0
 
-            loss = loss + ((spoof_weight * loss_fsteps) / (ctr_fsteps if ctr_fsteps > 0 else 1.0))
+            loss = loss + (
+                (spoof_weight * stream_loss_weight * loss_fsteps)
+                / (ctr_fsteps if ctr_fsteps > 0 else 1.0)
+            )
             ctr_streams += 1 if ctr_fsteps > 0 and not stream_is_spoof else 0
 
         # normalize by all targets and forecast steps that were non-empty
         # (with each having an expected loss of 1 for an uninitalized neural net)
         loss = loss / ctr_streams
 
+        def _nested_dict():
+            return defaultdict(dict)
+
+        # Reorder losses_all to [stream_name][loss_fct_name][ch_n][fstep]
+        reordered_losses = defaultdict(dict)
+        for stream_name, fstep_dict in losses_all.items():
+            reordered_losses[stream_name] = defaultdict(_nested_dict)
+            for fstep, lfct_dict in fstep_dict.items():
+                for loss_fct_name, ch_dict in lfct_dict.items():
+                    for ch_n, v in ch_dict.items():
+                        reordered_losses[stream_name][loss_fct_name][ch_n][fstep] = v
+
+        # Calculate per stream, per lfct average across channels and fsteps
+        for stream_name, lfct_dict in reordered_losses.items():
+            for loss_fct_name, ch_dict in lfct_dict.items():
+                reordered_losses[stream_name][loss_fct_name]["avg"] = 0
+                count = 0
+                for ch_n, fstep_dict in ch_dict.items():
+                    if ch_n != "avg":
+                        for _, v in fstep_dict.items():
+                            reordered_losses[stream_name][loss_fct_name]["avg"] += v
+                            count += 1
+                reordered_losses[stream_name][loss_fct_name]["avg"] /= count
+
         # Return all computed loss components encapsulated in a ModelLoss dataclass
-        return LossValues(loss=loss, losses_all=losses_all, stddev_all=None)
+        return LossValues(loss=loss, losses_all=reordered_losses, stddev_all=None)

src/weathergen/train/trainer.py
@@ -699,10 +699,11 @@ class Trainer(TrainerBase):
                     logger.info(pstr)
                     logger.info("\t")
 
-                for loss_name, loss_values in losses_all.items():
-                    logger.info(
-                        f"{loss_name} : {np.nanmean(loss_values['loss_avg']):0.4E} \t",
-                    )
+                for key, value in losses_all.items():
+                    if key.endswith("avg"):
+                        logger.info(
+                            f"{key} : {np.nanmean(value):0.4E} \t",
+                        )
                 logger.info("\n")
 
             self.t_start = time.time()

src/weathergen/utils/train_logger.py
@@ -22,7 +22,7 @@ import numpy as np
 import polars as pl
 
 import weathergen.common.config as config
-from weathergen.train.utils import flatten_dict, unflatten_dict
+from weathergen.train.utils import flatten_dict
 from weathergen.utils.distributed import ddp_average
 from weathergen.utils.metrics import get_train_metrics_path, read_metrics_file
 
@@ -120,10 +120,10 @@ class TrainLogger:
             metrics[_performance_gpu] = perf_gpu
             metrics[_performance_memory] = perf_mem
 
-        for key, value in flatten_dict(losses_all).items():
+        for key, value in losses_all.items():
             metrics[key] = np.nanmean(value)
 
-        for key, value in flatten_dict(stddev_all).items():
+        for key, value in stddev_all.items():
             metrics[key] = np.nanmean(value)
 
         self.log_metrics("train", metrics)
@@ -435,4 +435,4 @@ def prepare_losses_for_logging(
             if value:
                 stddev_all[key].append(ddp_average(value).item())
 
-    return real_loss, unflatten_dict(losses_all), unflatten_dict(stddev_all)
+    return real_loss, losses_all, stddev_all