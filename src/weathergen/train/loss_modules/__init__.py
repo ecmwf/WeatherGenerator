aa420b50b1fc45cb73066c0c1d3a3c1336d3f164
Christian Lessig <christian.lessig@ecmwf.int>
2025-12-17 20:20:50 +0100

    Removed mock-up

--- src/weathergen/train/loss_modules/__init__.py
+++ src/weathergen/train/loss_modules/__init__.py
@@ -7,27 +7,6 @@
 # granted to it by virtue of its status as an intergovernmental organisation
 # nor does it submit to any jurisdiction.
 
-from .loss_module_base import LossModuleBase, LossValues
 from .loss_module_physical import LossPhysical
 
-
-class LossLatentSSLStudentTeacher(LossModuleBase):
-    def __init__(
-        self,
-        cf,
-        loss_fcts: list,
-        stage,
-        device: str,
-    ):
-        LossModuleBase.__init__(self)
-
-    # a placeholder to test configs
-    def compute_loss(
-        self,
-        preds: dict,
-        targets: dict,
-    ) -> LossValues:
-        return LossValues(loss=0, losses_all={}, stddev_all={})
-
-
-__all__ = [LossPhysical, LossLatentSSLStudentTeacher]
+__all__ = [LossPhysical]
