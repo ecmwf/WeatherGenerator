4aa11eac32e86286c2f69f9472434a056fc37417
Christian Lessig <christian.lessig@ecmwf.int>
2026-01-09 11:14:38 +0100

    Fixed problem with undefined variable

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -546,7 +546,7 @@ class Trainer(TrainerBase):
 
                     pbar.update(self.batch_size_validation_per_gpu)
 
-                self._log_terminal(bidx, mini_epoch, VAL)
+                self._log_terminal( 0, mini_epoch, VAL)
                 self._log(VAL)
 
         # avoid that there is a systematic bias in the validation subset
