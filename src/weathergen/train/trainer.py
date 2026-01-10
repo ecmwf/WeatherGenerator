fd4ac9e3ccb53fd0af47578ee8357635d2ce20f1
Christian Lessig <christian.lessig@ecmwf.int>
2026-01-10 11:07:13 +0100

    Removed temporary patches; fixed properly in 10b7a28

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -98,14 +98,9 @@ class Trainer(TrainerBase):
         self.freeze_modules = cf.get("freeze_modules", "")
 
         self.training_cfg = cf.get("training_config")
-        # validation and test configs are training confforecastig, updated by specified keys
-        
+        # validation and test configs are training configs, updated by specified keys
         self.validation_cfg = merge_configs(self.training_cfg, cf.get("validation_config", {}))
         self.test_cfg = merge_configs(self.validation_cfg, cf.get("test_config", {}))
-
-        
-        cf.validation_config = self.validation_cfg
-        cf.test_config = self.test_cfg
 
         # batch sizes
         self.batch_size_per_gpu = get_batch_size_from_config(self.training_cfg)
