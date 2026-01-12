8f3721ddafbeba09ca4316f86edf4786e3524077
Michael Tarnawa <18899420+mtar@users.noreply.github.com>
2026-01-12 15:13:52 +0100

    update help message

--- src/weathergen/utils/cli.py
+++ src/weathergen/utils/cli.py
@@ -118,7 +118,7 @@ def _add_general_arguments(parser: argparse.ArgumentParser):
         type=Path,
         help=(
             "Path to the base configuration file."
-            "If not provided, the default configuration is used."
+            "If not provided, ./config/default_config.yml is used."
         )
     )
 
