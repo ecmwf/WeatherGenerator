bfeeacbde877ad472a65d4449d3488ebf61a25b1
sbAsma <semcheddineasma@yahoo.fr>
2026-01-13 08:43:23 +0100

    modification of forecasting engine initialization

--- src/weathergen/model/model.py
+++ src/weathergen/model/model.py
@@ -334,17 +334,13 @@ class Model(torch.nn.Module):
                 "Empty forecast engine (fe_num_blocks = 0), but forecast_steps[i] > 0 for some i"
             )
 
-        # check if diffusion mode is enabled
-        fe_diffusion_model = cf.get("fe_diffusion_model", False)
-        if fe_diffusion_model:
+        # Initialize forecasting engine: standard or diffusion-wrapped
+        self.forecast_engine = ForecastingEngine(cf, self.num_healpix_cells)
+        if cf.get("fe_diffusion_model", False):
             self.forecast_engine = DiffusionForecastEngine(
                 forecast_engine=ForecastingEngine(
                      cf, self.num_healpix_cells, forecast_engine=self.forecast_engine
                 )
-            )
-        else:
-            self.forecast_engine = ForecastingEngine(
-                cf, self.num_healpix_cells, forecast_engine=self.forecast_engine
             )
 
         ###############
