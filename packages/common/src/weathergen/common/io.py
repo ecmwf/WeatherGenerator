25a155cfff96f6076176a38e13b1ff4bf39c13f2
Sorcha <sorcha.owens@metoffice.gov.uk>
2026-01-15 14:42:09 +0100

    type-check errors

--- packages/common/src/weathergen/common/io.py
+++ packages/common/src/weathergen/common/io.py
@@ -409,7 +409,9 @@ class ZarrIO:
     def write_zarr(self, item: OutputItem):
         """Write one output item to the zarr store."""
         group = self._get_group(item.key, create=True)
-        for dataset in tqdm(item.datasets):
+        for dataset in tqdm(item.datasets): #pyrefly: ignore[not-iterable]
+            # pyrefly doesn't recognize that tqdm makes item.datasets iterable
+            # until fixed, ignore the warning here
             if dataset is not None:
                 self._write_dataset(group, dataset)
 
