Corrected latent token retrieval in loss calculation

src/weathergen/train/loss_modules/loss_module_latent_diffusion.py
@@ -91,8 +91,9 @@ class LossLatentDiffusion(LossModuleBase):
             for _, _, loss_fct_name in self.loss_fcts
         }
 
-        pred_tokens_all = [pl["latent_state"].latent_tokens for pl in preds.latent if pl]
-        target_tokens_all = [targets.latent]  # TODO: remove extra list
+
+        pred_tokens_all = [pl["latent_state"].z_pre_norm for pl in preds.latent if pl]
+        target_tokens_all = targets.latent
 
         eta = torch.tensor([targets.aux_outputs["noise_level_rn"]], device=self.device)
         fsteps = len(target_tokens_all)