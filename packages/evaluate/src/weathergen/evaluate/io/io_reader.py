9e912201cc7c7386da73600d7e73c2416992ce0b
s6sebusc <49226935+s6sebusc@users.noreply.github.com>
2026-01-20 14:44:57 +0100

    Skipping missing scores in JSONreader (#1655)
    * split WeatherGenReader functionality to allow reading only JSON
    
    adding weathergen JSON reader to develop
    
    * informative error when metrics are not there
    
    * restore JSONreader after rebase
    
    * JSONreader mostly restored
    
    * MLFlow logging independent of JSON/zarr
    
    * linting, properly cheking fsteps, ens, samples in JSONreader
    
    * tiny change to restore the MergeReader
    
    * lint
    
    * enabling JSONreader to skip plots and missing scores gracefully
    
    * required reformatting
    
    * move skipping of metrics to the reader class
    
    * slighly more explicit formulations
    
    ---------
    
    Co-authored-by: Sebastian Buschow <sbuschow@santis-ln001.cscs.ch>
    Co-authored-by: Sebastian Buschow <sbuschow@santis-ln002.cscs.ch>
    Co-authored-by: iluise <72020169+iluise@users.noreply.github.com>
    Co-authored-by: Ilaria Luise <luise.ilaria@gmail.com>

--- packages/evaluate/src/weathergen/evaluate/io/io_reader.py
+++ packages/evaluate/src/weathergen/evaluate/io/io_reader.py
@@ -223,7 +223,7 @@ class Reader:
                 if available_data is not None and reader_data[name] != available[name]:
                     self._logger.info(
                         f"Requested all {name}s for {mode}, but previous config was a "
-                        "strict subset. Recomputing."
+                        "strict subset. Recomputation required."
                     )
                     check_score = False
 
@@ -245,7 +245,8 @@ class Reader:
             if available_data is not None and not requested[name] <= available[name]:
                 missing = requested[name] - available[name]
                 self._logger.info(
-                    f"{name.capitalize()}(s) {missing} missing in previous evaluation. Recomputing."
+                    f"{name.capitalize()}(s) {missing} missing in previous evaluation."
+                    "Recomputation required."
                 )
                 check_score = False
 
