bcb781402745ca31e4e75e6818e54a5c67336940
Jubeku <julian.kuehnert@ecmwf.int>
2026-02-05 22:20:38 +0100

    fix sharding when FE None

--- src/weathergen/model/model_interface.py
+++ src/weathergen/model/model_interface.py
@@ -110,9 +110,10 @@ def init_model_and_shard(
             if isinstance(module, modules_to_shard):
                 fully_shard(module, **fsdp_kwargs)
 
-        for module in model.forecast_engine.fe_blocks.modules():
-            if isinstance(module, modules_to_shard):
-                fully_shard(module, **fsdp_kwargs)
+        if model.forecast_engine:
+            for module in model.forecast_engine.fe_blocks.modules():
+                if isinstance(module, modules_to_shard):
+                    fully_shard(module, **fsdp_kwargs)
 
         for module in model.latent_heads.modules():
             if isinstance(module, modules_to_shard):
