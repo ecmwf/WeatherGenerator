d8d5269771a29a06f8158682ba09314c2fbb4b11
sbAsma <a.semcheddine@fz-juelich.de>
2026-02-06 15:33:46 +0100

    fixed file not found

--- packages/evaluate/src/weathergen/evaluate/io/wegen_reader.py
+++ packages/evaluate/src/weathergen/evaluate/io/wegen_reader.py
@@ -320,8 +320,8 @@ class WeatherGenZarrReader(WeatherGenReader):
             ):
                 self.fname_zarr = fname_zarr
         else:
-            _logger.error(f"Zarr file {self.fname_zarr} does not exist.")
-            raise FileNotFoundError(f"Zarr file {self.fname_zarr} does not exist")
+            _logger.error(f"Zarr file {fname_zarr} does not exist.")
+            raise FileNotFoundError(f"Zarr file {fname_zarr} does not exist")
 
     def get_data(
         self,
