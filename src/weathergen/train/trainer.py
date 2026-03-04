d6d1ebb72fc91c6376be0d5b9778d7e6dd8f8da0
Sophie Xhonneux <sophiex@Sophies-MacBook-Pro.local>
2026-03-04 18:15:34 +0100

    Add alternative Local2Global + fix grad norm comp

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -757,7 +757,7 @@ class Trainer(TrainerBase):
         grad_norms = {"grad_norm.total": self.last_grad_norm}
         for name, param in self.model.named_parameters():
             if param.grad is not None:
-                grad_norms["grad_norm." + name] = self._get_tensor_item(param.grad.norm())
+                grad_norms["grad_norm." + name] = self._get_tensor_item(param.grad.norm()/ torch.sqrt(param.numel))
 
         if is_root():
             self.train_logger.log_metrics(stage, grad_norms)
