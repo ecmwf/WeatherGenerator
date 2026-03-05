03150540cd576ebe12b5f0fe5d7935bbf90c2981
Tim Hunter <tim.hunter@ecmwf.int>
2026-03-05 10:08:25 +0100

    fix test

--- integration_tests/small1_test.py
+++ integration_tests/small1_test.py
@@ -55,8 +55,6 @@ def test_train(setup, test_run_id):
             f"--config={WEATHERGEN_HOME}/integration_tests/small1.yaml",
             "--run-id",
             test_run_id,
-            "--from-run-id",
-            test_run_id,
         ]
     )
 
