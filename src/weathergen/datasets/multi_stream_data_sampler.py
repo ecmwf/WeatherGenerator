d9a83caec1eb123adf0a7428c1d77f9f2e7a34ec
lessig2 <christian.lessig@ecmwf.int>
2026-03-11 12:51:40 +0100

    Fixed some corner cases in handling of when batch samples are NaN and when batch is empty for latent loss

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -752,8 +752,14 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
 
                 batch = self._get_batch(idx, num_forecast_steps)
 
+                # ensure the batch is valid, i.e. not completely empty and no NaN values
+                # student teacher has no classical targets
+                mode = self.mode_cfg.get("training_mode")
+                is_valid = batch.sources_empty() or batch.is_nan()
+                is_valid = is_valid or (batch.targets_empty() if "masking" in mode else True)
+
                 # skip completely empty batch item or when all targets are empty -> no grad
-                if batch.is_empty() or batch.is_nan():
+                if is_valid:
                     logger.warning(f"Skipping empty batch with idx={idx}.")
                 else:
                     break
