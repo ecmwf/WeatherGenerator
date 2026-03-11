e163c525f88d55a31ccd82c255abfd3d6a0e5203
lessig2 <christian.lessig@ecmwf.int>
2026-03-11 19:56:45 +0100

    Fixed handling of when batch valid is

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -755,11 +755,11 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                 # ensure the batch is valid, i.e. not completely empty and no NaN values
                 # student teacher has no classical targets
                 mode = self.mode_cfg.get("training_mode")
-                is_valid = batch.sources_empty() or batch.is_nan()
-                is_valid = is_valid or (batch.targets_empty() if "masking" in mode else True)
+                not_valid = batch.sources_empty() or batch.is_nan()
+                not_valid = not_valid or (batch.targets_empty() if "masking" in mode else False)
 
                 # skip completely empty batch item or when all targets are empty -> no grad
-                if is_valid:
+                if not_valid:
                     logger.warning(f"Skipping empty batch with idx={idx}.")
                 else:
                     break
