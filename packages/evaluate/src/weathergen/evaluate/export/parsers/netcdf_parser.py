e1545e897cb629851c12a749aae98d963b4dc96d
Michael Tarnawa <18899420+mtar@users.noreply.github.com>
2026-03-16 11:22:10 +0100

    linting

--- packages/evaluate/src/weathergen/evaluate/export/parsers/netcdf_parser.py
+++ packages/evaluate/src/weathergen/evaluate/export/parsers/netcdf_parser.py
@@ -73,7 +73,7 @@ class NetcdfParser(CfParser):
                 continue
 
             result = result.as_xarray().squeeze()
-            if "channel" not in result.indexes: 
+            if "channel" not in result.indexes:
                 result = result.expand_dims("channel")
             result = result.sel(channel=self.channels)
             result = self.reshape(result)
