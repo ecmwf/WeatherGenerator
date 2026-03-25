52acede2a5fbb102b400dd857c6237641b1f812b
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-03-25 17:24:15 +0100

    debug refactor

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -163,7 +163,9 @@ class Trainer(TrainerBase):
         collapse_config = cf.train_logging.get("collapse_monitoring", {})
         self.collapse_monitor = CollapseMonitor(collapse_config, None)  # device set later in run()
 
-        available_flops = get_available_flops(self.devices[0], dtype=self.mixed_precision_dtype) # Assuming same device type!
+        device = self.devices[0]
+        device.type = device
+        available_flops = get_available_flops(device, dtype=self.mixed_precision_dtype) # Assuming same device type!
         self.throughput = Throughput(
             available_flops=available_flops,
             world_size=self.world_size_original,
