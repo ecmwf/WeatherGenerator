8ab3e0ea7688d5dfbd0fd72a9a3750e6c4681504
melidonis1 <s.melidonis@fz-juelich.de>
2026-04-01 11:11:59 +0200

    Linting

--- packages/evaluate/src/weathergen/evaluate/plotting/plotter.py
+++ packages/evaluate/src/weathergen/evaluate/plotting/plotter.py
@@ -18,8 +18,8 @@ from matplotlib.collections import LineCollection
 from matplotlib.lines import Line2D
 from PIL import Image
 from scipy.stats import wilcoxon
-from weathergen.common.config import _load_private_conf
 
+from weathergen.common.config import _load_private_conf
 from weathergen.evaluate.plotting.plot_utils import (
     DefaultMarkerSize,
 )
