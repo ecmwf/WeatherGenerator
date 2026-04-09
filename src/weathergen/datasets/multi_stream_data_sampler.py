bc6a73764f658d13608ab6a69b54fb2c1bf03529
lessig2 <christian.lessig@ecmwf.int>
2026-04-09 22:18:42 +0200

    Fixed handling of spoofing with forecasting. More testing needed.

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -390,8 +390,6 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                 rdata = input_data[-(step + 1)]
                 token_data = input_tokens[-(step + 1)]
 
-                stream_data.source_is_spoof = rdata.is_spoof
-
                 # preprocess data for model input
                 (source_cells, source_cells_lens) = self.tokenizer.get_source(
                     stream_info,
@@ -432,8 +430,6 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
             rdata = output_data[step]
             token_data = output_tokens[step]
 
-            stream_data.target_is_spoof = rdata.is_spoof
-
             if "target_coords" in mode:
                 (tc, tc_l) = self.tokenizer.get_target_coords(
                     stream_info,
@@ -442,7 +438,7 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                     (time_win_target.start, time_win_target.end),
                     target_mask,
                 )
-                stream_data.add_target_coords(timestep_idx, tc, tc_l)
+                stream_data.add_target_coords(timestep_idx, tc, tc_l, rdata.is_spoof)
 
             if "target_values" in mode:
                 (tt_cells, tt_t, tt_c, idxs_inv) = self.tokenizer.get_target_values(
@@ -452,7 +448,9 @@ class MultiStreamDataSampler(torch.utils.data.IterableDataset):
                     (time_win_target.start, time_win_target.end),
                     target_mask,
                 )
-                stream_data.add_target_values(timestep_idx, tt_cells, tt_c, tt_t, idxs_inv)
+                stream_data.add_target_values(
+                    timestep_idx, tt_cells, tt_c, tt_t, idxs_inv, rdata.is_spoof
+                )
 
         return stream_data
 
