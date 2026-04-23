5ec9adaae795d45ccc04e27ee458181957af7ce7
Savvas Melidonis <s.melidonis@fz-juelich.de>
2026-04-23 12:18:02 +0200

    Linting

--- packages/evaluate/src/weathergen/evaluate/plotting/plotter.py
+++ packages/evaluate/src/weathergen/evaluate/plotting/plotter.py
@@ -18,8 +18,8 @@ from cartopy.io import DownloadWarning
 from matplotlib.collections import LineCollection
 from matplotlib.lines import Line2D
 from scipy.stats import wilcoxon
-from weathergen.common.config import _load_private_conf
 
+from weathergen.common.config import _load_private_conf
 from weathergen.evaluate.plotting.plot_utils import DefaultMarkerSize
 from weathergen.evaluate.utils.regions import RegionBoundingBox
 
