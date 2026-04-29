6a4f715b16ef4b57792851e640f62f638b586233
iluise <luise.ilaria@gmail.com>
2026-04-29 17:55:58 +0200

    Refactor: extract plot classes from plotter.py into separate modules
    - Remove BarPlots, LinePlots, QuantilePlots, ScoreCards classes from plotter.py (already in separate files from #2140)
    - Move utility functions (format_datetime, calculate_average_over_dim, etc.) to plot_utils.py
    - Update plot_orchestration.py to import from separate modules
    - Add plotting __init__.py
    - plotter.py now contains only the Plotter base class

--- packages/evaluate/src/weathergen/evaluate/plotting/plot_orchestration.py
+++ packages/evaluate/src/weathergen/evaluate/plotting/plot_orchestration.py
@@ -32,13 +32,11 @@ from weathergen.evaluate.plotting.plot_utils import (
     ratio_plot_metric_region,
     score_card_metric_region,
 )
-from weathergen.evaluate.plotting.plotter import (
-    BarPlots,
-    LinePlots,
-    Plotter,
-    QuantilePlots,
-    ScoreCards,
-)
+from weathergen.evaluate.plotting.bar_plots import BarPlots
+from weathergen.evaluate.plotting.line_plots import LinePlots
+from weathergen.evaluate.plotting.plotter import Plotter
+from weathergen.evaluate.plotting.quantile_plots import QuantilePlots
+from weathergen.evaluate.plotting.score_cards import ScoreCards
 from weathergen.evaluate.scores.score import VerifiedData, get_score
 from weathergen.evaluate.scores.score_orchestration import get_next_fstep_data
 from weathergen.evaluate.utils.array_utils import bias_ranges, common_ranges
