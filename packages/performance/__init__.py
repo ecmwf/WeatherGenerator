c5af276320063b90bbf4467ba4de0a5873f01963
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-05-04 11:03:33 +0200

    init refactor

--- packages/performance/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Performance analysis tools for WeatherGenerator.
-
-This package contains tools for extracting and analyzing scaling performance data.
-"""
-
-__version__ = "0.1.0"
--- packages/performance/src/performance/__init__.py
+++ packages/performance/src/performance/__init__.py
@@ -1,3 +1,1 @@
-"""WeatherGenerator performance analysis package."""
-
-__all__ = ["extract_scaling_data", "generate_scaling_plots"]
+"""WeatherGenerator performance analysis tools."""
