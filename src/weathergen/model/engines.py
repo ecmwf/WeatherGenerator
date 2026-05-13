5a568d44d2edef07e3df1f8785270ec9db9ea21f
Kacper Nowak <kacper.nowak@awi.de>
2026-05-13 17:42:45 +0200

    Linter

--- src/weathergen/model/engines.py
+++ src/weathergen/model/engines.py
@@ -113,7 +113,8 @@ class EmbeddingEngine(torch.nn.Module):
         max_tokens = self.cf.get("ae_local_max_tokens_per_cell", 64)
         assert (
             batch.tokens_lens.flatten(0, 2).sum(0).max() <= max_tokens
-        ), "max number of tokens per cell for positional encoding exceeded. Increase ae_local_max_tokens_per_cell."
+        ), "max number of tokens per cell for positional encoding exceeded."
+        " Increase ae_local_max_tokens_per_cell in config."
 
         if batch.tokens_lens.shape[2] == 1:
             # trivial with one stream
