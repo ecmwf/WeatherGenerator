78a2c51a33a5dc90cd61e21f960e99654c630aff
melidonis1 <s.melidonis@fz-juelich.de>
2026-05-17 20:08:31 +0200

    Minor correction in logic

--- packages/evaluate/src/weathergen/evaluate/run_evaluation.py
+++ packages/evaluate/src/weathergen/evaluate/run_evaluation.py
@@ -25,6 +25,13 @@ from omegaconf import DictConfig, OmegaConf, open_dict
 from weathergen.common.logger import init_loggers
 from weathergen.common.paths import _REPO_ROOT
 from weathergen.common.platform_env import get_platform_env
+from weathergen.metrics.mlflow_utils import (
+    MlFlowUpload,
+    get_or_create_mlflow_parent_run,
+    log_scores,
+    setup_mlflow,
+)
+
 from weathergen.evaluate.io.csv_reader import CsvReader
 from weathergen.evaluate.io.merge_reader import WeatherGenMergeReader
 from weathergen.evaluate.io.wegen_reader import (
@@ -43,12 +50,6 @@ from weathergen.evaluate.scores.score_orchestration import (
     metric_list_to_json,
 )
 from weathergen.evaluate.utils.dict_utils import merge, parse_metric_params, triple_nested_dict
-from weathergen.metrics.mlflow_utils import (
-    MlFlowUpload,
-    get_or_create_mlflow_parent_run,
-    log_scores,
-    setup_mlflow,
-)
 
 _DEFAULT_PLOT_DIR = _REPO_ROOT / "plots"
 
@@ -317,10 +318,8 @@ def evaluate_from_config(cfg: dict, mlflow_client: MlflowClient | None) -> None:
             run["max_workers"] = max_workers
 
         for stream in run.get("streams", {}):
-            regions = (
-                run.get("streams", {})
-                .get(stream, {})
-                .get("regions", cfg.evaluation.get("regions", ["global"]))
+            regions = cfg.evaluation.get(
+                "regions", run.get("streams", {}).get(stream, {}).get("regions", ["global"])
             )
             tasks.append(
                 {
