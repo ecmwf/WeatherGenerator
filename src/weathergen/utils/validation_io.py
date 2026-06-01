f5aaadad6d5a27c3d2e5eaeef4fc62f0a767e053
Christian Lessig <christian.lessig@ecmwf.int>
2026-06-01 08:31:15 +0200

    Fixes needed to run inference and write output with spoofed streams

--- src/weathergen/utils/validation_io.py
+++ src/weathergen/utils/validation_io.py
@@ -57,13 +57,12 @@ def write_output(
             # handle spoof data: do not write since it might corrupt validation (spoofing invisible
             # there)
             if target_aux_out.physical[t_idx][sname]["is_spoof"][0]:
-                preds = model_output.get_physical_prediction(t_idx, sname)
-                preds_shape = preds[0].shape
+                targets = target_aux_out.physical[t_idx][sname]["target"]
                 # for-loop to make sure we have a consistent number of samples
-                preds_s = [np.zeros((preds_shape[0], 0, preds_shape[2])) for _ in preds]
-                targets_s = [np.zeros((0, preds_shape[2])) for _ in preds]
-                t_coords_s = [np.zeros((0, 2)) for _ in preds]
-                t_times_s = [np.array([]).astype("datetime64[ns]") for _ in preds]
+                preds_s = [np.zeros((1, 0, t.shape[1])) for t in targets]
+                targets_s = [np.zeros((0, t.shape[1])) for t in targets]
+                t_coords_s = [np.zeros((0, 2)) for t in targets]
+                t_times_s = [np.array([]).astype("datetime64[ns]") for t in targets]
 
             else:
                 preds = model_output.get_physical_prediction(t_idx, sname)
@@ -132,7 +131,8 @@ def write_output(
         output_stream_names = stream_names
 
     output_streams = {name: stream_names.index(name) for name in output_stream_names}
-    _logger.debug(f"Using output streams: {output_streams} from streams: {stream_names}")
+    if batch_idx == 0:
+        _logger.info(f"Using output streams: {output_streams} from streams: {stream_names}")
 
     target_channels: list[list[str]] = [list(stream.val_target_channels) for stream in cf.streams]
     source_channels: list[list[str]] = [list(stream.val_source_channels) for stream in cf.streams]
