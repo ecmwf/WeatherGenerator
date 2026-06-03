705ef6b85b806bf34292c19f180981ef8843ca7e
Christian Lessig <christian.lessig@ecmwf.int>
2026-06-03 08:27:21 +0200

    Linting

--- src/weathergen/datasets/multi_stream_data_sampler.py
+++ src/weathergen/datasets/multi_stream_data_sampler.py
@@ -204,7 +204,7 @@ Set repeat_data_in_mini_epoch to True if this is undesired."
         perms_len = int(self.index_range.end - self.index_range.start)
         perms_len -= (fsm + self.output_offset) * (self.time_step // self.step_timedelta)
 
-        return np.arange( perms_len)
+        return np.arange(perms_len)
 
     def _init_stream_datasets(self, cf) -> dict[StreamName, list[AnyDataReader]]:
         """Load dataset readers for all streams from config."""
