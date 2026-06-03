672309d890f6095339d519134ed36d41e463a0c4
Sophie X <24638638+sophie-xhonneux@users.noreply.github.com>
2026-06-03 14:49:19 +0200

    Spoof two tokens in stream_data.py
    Add random selection of two coordinates to reduce load

--- src/weathergen/datasets/stream_data.py
+++ src/weathergen/datasets/stream_data.py
@@ -452,6 +452,9 @@ def spoof(healpix_level: int, datetime, geoinfo_size, num_channels) -> IOReaderD
         np.arange(0, num_healpix_cells), 2**healpix_level, dx=dx, dy=dy, order="nested"
     )
     coords = np.stack([lats.deg, lons.deg], axis=-1, dtype=np.float32)
+    # spoof two tokens to avoid unnecessary computational load
+    coords = coords[np.random.choice(coords.shape[0], size=2, replace=False)]
+    
     geoinfos = np.zeros((coords.shape[0], geoinfo_size), dtype=np.float32)
 
     data = np.zeros((coords.shape[0], num_channels), dtype=np.float32)
