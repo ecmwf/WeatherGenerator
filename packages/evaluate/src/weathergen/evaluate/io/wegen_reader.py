5c335a5a5d0f6a1684a0c3df8d8090c26b7f8639
lessig2 <christian.lessig@ecmwf.int>
2026-06-09 21:24:16 +0200

    Fix missing type of rnaks when specified as list

--- packages/evaluate/src/weathergen/evaluate/io/wegen_reader.py
+++ packages/evaluate/src/weathergen/evaluate/io/wegen_reader.py
@@ -12,6 +12,7 @@ import json
 import logging
 from collections import defaultdict
 from pathlib import Path
+import omegaconf
 
 # Third-party
 import numpy as np
@@ -434,7 +435,7 @@ class WeatherGenZarrReader(WeatherGenReader):
             _logger.info(f"Discovered {len(files)} rank file(s) for run {self.run_id}.")
             return self._validate_rank_files(files)
 
-        elif isinstance(rank_cfg, list | tuple):
+        elif isinstance(rank_cfg, list | tuple | omegaconf.listconfig.ListConfig):
             files = []
             for r in rank_cfg:
                 fname = self.results_dir / (
