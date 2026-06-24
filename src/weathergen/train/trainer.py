a589eff7d5d4bb78f30bccaae4799565427bcf00
Florian Scheidl <f.scheidl@fz-juelich.de>
2026-06-24 11:05:26 +0200

    Keep grad scaling by default for now

--- src/weathergen/train/trainer.py
+++ src/weathergen/train/trainer.py
@@ -332,7 +332,7 @@ class Trainer(TrainerBase):
             betas=(beta1, beta2),
             eps=eps,
         )
-        if cf.grad_scaling:
+        if cf.get("grad_scaling", True):
             self.grad_scaler = torch.amp.GradScaler("cuda")
         assert len(self.dataset) > 0, f"No data found in {self.dataset}"
 
