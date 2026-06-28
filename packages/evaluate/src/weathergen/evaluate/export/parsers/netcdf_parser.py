2ac1e91ec0b2adbb5874dba54fc094d137cbb251
Sorcha Owens <sorcha.owens@metoffice.gov.uk>
2026-06-28 14:22:28 +0200

    variable handling fix

--- packages/evaluate/src/weathergen/evaluate/export/parsers/netcdf_parser.py
+++ packages/evaluate/src/weathergen/evaluate/export/parsers/netcdf_parser.py
@@ -345,6 +345,7 @@ class NetcdfParser(CfParser):
             coords = self._build_coordinate_mapping(ds, mapped_info, ds_attrs)
 
             attributes = {
+                "GRIB_shortName": mapped_name, # if GRIB
                 "standard_name": mapped_info.get("std", var_name),
                 "units": mapped_info.get("std_unit", "unknown"),
             }
@@ -557,6 +558,7 @@ class NetcdfParser(CfParser):
 class GribParser(NetcdfParser):
     """
     Child class for handling GRIB output format.
+    Important to note it must be used with regridding to regular_ll
     """
     def gribify(self, ds: xr.Dataset) -> xr.Dataset:
         """
@@ -570,6 +572,55 @@ class GribParser(NetcdfParser):
         if "forecast_period" in ds.coords:
             ds = ds.rename({"forecast_period": "step"})
         return ds
+    
+    def _attrs_gaussian_grid(self, ds: xr.Dataset) -> xr.Dataset:
+        """
+        Assign CF-compliant attributes to variables in a Gaussian grid dataset.
+        Parameters
+        ----------
+            ds : xr.Dataset
+                Input dataset.
+        Returns
+        -------
+            xr.Dataset
+                Dataset with CF-compliant variable attributes.
+        """
+        variables = {}
+        dims_cfg = self.config.get("dimensions", {})
+        ds, ds_attrs = self._assign_dim_attrs(ds, dims_cfg)
+        for var_name, da in ds.data_vars.items():
+            mapped_info = self.mapping.get(var_name, {})
+            mapped_name = mapped_info.get("var", var_name)
+
+            coords = self._build_coordinate_mapping(ds, mapped_info, ds_attrs)
+
+            # parse grib specifc short names
+            GRIB_shortnames = {"t2m": "2t",
+             "u10": "10u",
+             "v10": "10v",
+             "d2m" : "2d",}
+            GRIB_levels_special = {"t2m": 2, "u10": 10, "v10": 10, "d2m": 2}
+
+            attributes = {
+                "GRIB_shortName": GRIB_shortnames.get(mapped_name, mapped_name), # if GRIB
+                "standard_name": mapped_info.get("std", var_name),
+                "units": mapped_info.get("std_unit", "unknown"),
+            }
+            if mapped_name in GRIB_shortnames.keys():
+                attributes.update({
+                "GRIB_typeOfLevel": "heightAboveGround",
+                "GRIB_level": GRIB_levels_special[mapped_name],})
+            if "long" in mapped_info:
+                attributes["long_name"] = mapped_info["long"]
+            variables[mapped_name] = xr.DataArray(
+                data=da.values,
+                dims=da.dims,
+                coords=coords,
+                attrs=attributes,
+                name=mapped_name,
+            )
+
+        return variables
 
     def save(self, ds: xr.Dataset, forecast_ref_time: np.datetime64) -> None:
         """
@@ -588,5 +639,6 @@ class GribParser(NetcdfParser):
         out_fname = self.get_output_filename(forecast_ref_time)
         _logger.info(f"Saving to {out_fname}.")
         ds = self.gribify(ds)
+        print(ds)
         to_grib(ds, out_fname)
         _logger.info(f"Saved GRIB file to {out_fname}.")
