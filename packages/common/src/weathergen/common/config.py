db1df7b2223fa7b716d05541c4fdd77ba8513d09
lessig2 <christian.lessig@ecmwf.int>
2026-08-06 12:53:53 +0200

    Revert change

--- packages/common/src/weathergen/common/config.py
+++ packages/common/src/weathergen/common/config.py
@@ -140,7 +140,7 @@ def _strip_interpolation(conf: Config) -> Config:
     stripped = {}
     if OmegaConf.is_dict(conf):
         for key in list(conf.keys()):
-            # key = str(key) # this lines breaks target_source_correspondence please stop re introducing it
+            key = str(key)
             if OmegaConf.is_missing(conf, key):
                 val = "???"
             elif OmegaConf.is_config(conf[key]):
