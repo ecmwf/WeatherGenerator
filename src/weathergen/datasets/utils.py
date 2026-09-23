ea984799a4c326bb9fd74228feb5a70e36425f3b
kasravi1 <j.kasravi@fz-jeulich.de>
2026-09-23 11:18:06 +0200

    Remove unused locs_to_ctr_coords and locs_to_cell_coords_ctrs

--- src/weathergen/datasets/utils.py
+++ src/weathergen/datasets/utils.py
@@ -126,37 +126,6 @@ def locs_to_cell_coords(hl: int, locs: list, dx=0.5, dy=0.5) -> list:
 
 
 ####################################################################################################
-def locs_to_ctr_coords(ctrs_r3, locs: list[torch.Tensor]) -> list:
-    """
-    Map a list of locations per cell to spherical local coordinates centered
-    at the healpix cell center
-    """
-
-    ctrs_rots = vecs_to_rots(ctrs_r3).to(torch.float32)
-
-    ## express each centroid in local coordinates w.r.t to healpix center
-    #  by rotating center to origin
-
-    # Concatenate all points into single tensor
-    all_points = torch.cat(locs, dim=0)
-
-    lengths = torch.tensor([len(s) for s in locs], device=all_points.device)
-    batch_indices = torch.repeat_interleave(
-        torch.arange(len(locs), device=all_points.device), lengths
-    )
-
-    point_rotations = ctrs_rots[batch_indices]
-
-    # Single vectorized batch matrix multiplication
-    rotated_points = torch.bmm(point_rotations, all_points.unsqueeze(-1)).squeeze(-1)
-
-    # Split back using tensor operations
-    local_locs = torch.split(rotated_points, lengths.tolist())
-
-    return list(local_locs)
-
-
-####################################################################################################
 def healpix_verts(hl: int, dx=0.5, dy=0.5):
     """
     healpix cell center
@@ -187,36 +156,6 @@ def healpix_verts_rots(hl: int, dx=0.5, dy=0.5):
     verts_rot3 = vecs_to_rots(verts)
 
     return verts, verts_rot3
-
-
-####################################################################################################
-def locs_to_cell_coords_ctrs(
-    healpix_centers_rots: torch.Tensor, locs: list[torch.Tensor]
-) -> torch.Tensor:
-    """
-    Map a list of locations per cell to spherical local coordinates centered
-    at the healpix cell center
-    """
-
-    ## express each centroid in local coordinates w.r.t to healpix center
-    #  by rotating center to origin
-
-    # Concatenate all non-empty locations
-    all_points = torch.cat(locs, dim=0)
-    lengths = torch.tensor([len(s) for s in locs], device=all_points.device)
-
-    # Efficiently create batch indices using torch.repeat_interleave
-    batch_indices = torch.repeat_interleave(
-        torch.arange(len(locs), device=all_points.device), lengths
-    )
-
-    # Select rotation matrices for each point
-    rotations_selected = healpix_centers_rots[batch_indices]
-
-    # Vectorized matrix multiplication
-    local_locs = torch.bmm(rotations_selected, all_points.unsqueeze(-1)).squeeze(-1)
-
-    return local_locs
 
 
 ####################################################################################################
